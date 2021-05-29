ff2cae9d06d907304309402881b93fa2efca1235
johnathan White <jwhite@bitpay.com>
2021-05-29 15:26:33 -0400

    added styled components; updated splash screen; added colors constants

--- src/navigation/app/screens/Splash.tsx
+++ src/navigation/app/screens/Splash.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import styled from 'styled-components/native';
+import BitPayLogo from '../../../assets/img/logos/bitpay-white.svg';
+import {BitPay} from '../../../styles/colors';
 
+const SplashContainer = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background: ${BitPay};
+`;
+
+// TODO animate me
 const SplashScreen = () => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Splash Screen</Text>
-    </View>
+    <SplashContainer>
+      <BitPayLogo width={125} height={125} />
+    </SplashContainer>
   );
 };
 
---
+++ src/styles/colors.ts
@@ -0,0 +1,20 @@
+/* Primary */
+export const Midnight = '#0C204E';
+export const BitPay = '#1A3B8B';
+export const Action = '#4F6EF7';
+
+/* Secondary */
+export const SlateDark = '#434D5A';
+export const Slate = '#9BA3AE';
+export const Air = '#EBEDF8';
+export const Feather = '#F6F7FC';
+
+/* Neutrals */
+export const Black = '#303133';
+export const Grey = '#E7E7E7';
+export const White = '#FFFFFF';
+
+/* Interface */
+export const Caution = '#ef476f';
+export const Success = '#2fcfa4';
+export const Warning = '#fdb455';
