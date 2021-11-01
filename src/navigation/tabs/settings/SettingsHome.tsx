0eee779b64e2df11440cfbbb2604cd34fe774d7a
Chris Nguyen <cnguyen@bitpay.com>
2021-11-01 13:28:24 -0700

    Add initial commit for Settings/Session Logs screen

---
+++ src/navigation/tabs/settings/SettingsHome.tsx
@@ -0,0 +1,17 @@
+import { NavigationProp, useNavigation } from '@react-navigation/core';
+import React from 'react';
+import { Text, View } from 'react-native';
+import { SettingsStackParamList } from './SettingsStack';
+
+const SettingsIndex: React.FC = () => {
+  const navigation = useNavigation<NavigationProp<SettingsStackParamList>>();
+
+  return <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <Text>Settings!</Text>
+    <Text onPress={() => navigation.navigate('SessionLog')}>
+      Session Log
+    </Text>
+  </View>
+};
+
+export default SettingsIndex;
