ac6a7f2da694b65af5b2aeca2446defccc98102d
Chris Nguyen <cnguyen@bitpay.com>
2021-11-03 08:14:26 -0700

    Add base screen options to SettingsStack

--- src/navigation/tabs/settings/SettingsStack.tsx
+++ src/navigation/tabs/settings/SettingsStack.tsx
@@ -2,6 +2,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import SettingsHome from './SettingsHome';
 import SessionLog from './SessionLog';
+import { baseNavigatorOptions, baseScreenOptions } from '../../../constants/NavigationOptions';
 
 export type SettingsStackParamList = {
   Home: undefined;
@@ -12,7 +13,10 @@ const SettingsStack = () => {
   const Settings = createStackNavigator<SettingsStackParamList>();
 
   return (
-    <Settings.Navigator initialRouteName="Home">
+    <Settings.Navigator initialRouteName="Home" screenOptions={{
+      ...baseNavigatorOptions,
+      ...baseScreenOptions
+    }}>
       <Settings.Screen name="Home" component={SettingsHome} options={{
         title: 'Settings'
       }} />
