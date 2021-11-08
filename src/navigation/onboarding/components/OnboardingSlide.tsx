d3642ad5102e551506e275b83e50d3c594de2756
Chris Nguyen <cnguyen@bitpay.com>
2021-11-08 12:29:46 -0800

    Add theming to OnboardingSlide

--- src/navigation/onboarding/components/OnboardingSlide.tsx
+++ src/navigation/onboarding/components/OnboardingSlide.tsx
@@ -1,9 +1,4 @@
-import {
-  Disclaimer,
-  H3,
-  Paragraph,
-  TextAlign,
-} from '../../../components/styled/Text';
+import { useTheme } from '@react-navigation/native';
 import React, {ReactElement} from 'react';
 import styled from 'styled-components/native';
 import {
@@ -12,6 +7,12 @@ import {
   TextContainer,
   TitleContainer,
 } from '../../../components/styled/Containers';
+import {
+  Disclaimer,
+  H3,
+  Paragraph,
+  TextAlign,
+} from '../../../components/styled/Text';
 
 interface OnboardingSlide {
   title: string;
@@ -21,7 +22,7 @@ interface OnboardingSlide {
 }
 
 const SlideContainer = styled.View`
-  background: white;
+  background: transparent;
   justify-content: center;
   align-items: center;
   margin-top: 20px;
@@ -29,23 +30,25 @@ const SlideContainer = styled.View`
 
 export const OnboardingSlide = ({item}: {item: OnboardingSlide}) => {
   const {title, text, subText, img} = item;
+  const theme = useTheme();
+  const themedText = { color: theme.colors.text };
 
   return (
     <SlideContainer>
       <ImageContainer>{img()}</ImageContainer>
       <TitleContainer>
-        <TextAlign align={'center'}>
+        <TextAlign align={'center'} style={themedText}>
           <H3>{title}</H3>
         </TextAlign>
       </TitleContainer>
       <TextContainer>
-        <TextAlign align={'center'}>
+        <TextAlign align={'center'} style={themedText}>
           <Paragraph>{text}</Paragraph>
         </TextAlign>
       </TextContainer>
       {subText && (
         <SubTextContainer>
-          <TextAlign align={'center'}>
+          <TextAlign align={'center'} style={themedText}>
             <Disclaimer>{subText}</Disclaimer>
           </TextAlign>
         </SubTextContainer>
