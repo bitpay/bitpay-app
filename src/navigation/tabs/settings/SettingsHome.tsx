59435e65289d17442c93e7f98c99534403f5bab2
Chris Nguyen <cnguyen@bitpay.com>
2021-11-09 09:19:05 -0800

    Add basic Theme page

--- src/navigation/tabs/settings/SettingsHome.tsx
+++ src/navigation/tabs/settings/SettingsHome.tsx
@@ -1,15 +1,19 @@
 import {NavigationProp, useNavigation} from '@react-navigation/core';
+import { useTheme } from '@react-navigation/native';
 import React from 'react';
-import {Text, View} from 'react-native';
-import {SettingsStackParamList} from './SettingsStack';
+import {Button, Text, View} from 'react-native';
+import {SettingsScreens, SettingsStackParamList} from './SettingsStack';
 
 const SettingsIndex: React.FC = () => {
   const navigation = useNavigation<NavigationProp<SettingsStackParamList>>();
+  const theme = useTheme();
+  const textStyle = { color: theme.colors.text };
 
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Settings!</Text>
-      <Text onPress={() => navigation.navigate('SessionLog')}>Session Log</Text>
+      <Text style={textStyle}>Settings!</Text>
+      <Button title="Theme" onPress={() => navigation.navigate(SettingsScreens.THEME)} />
+      <Button title="Session Log" onPress={() => navigation.navigate(SettingsScreens.SESSION_LOG)} />
     </View>
   );
 };
