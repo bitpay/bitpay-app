9f73809abd9b0c48443ba5b89105d03da3a61aa4
Chris Nguyen <cnguyen@bitpay.com>
2021-11-09 09:22:58 -0800

    Add theming to SessionLog

--- src/navigation/tabs/settings/SessionLog.tsx
+++ src/navigation/tabs/settings/SessionLog.tsx
@@ -1,5 +1,6 @@
+import { useTheme } from '@react-navigation/native';
 import React, {useState} from 'react';
-import {Text} from 'react-native';
+import {StyleProp, Text, TextStyle} from 'react-native';
 import {useSelector} from 'react-redux';
 import {RootState} from '../../../store';
 import {LogEntry, LogLevel} from '../../../store/log/log.models';
@@ -15,6 +16,8 @@ const LogDisplay = ({log}: {log: LogEntry}) => {
 };
 
 const SessionLogs: React.FC = () => {
+  const theme = useTheme();
+  const textStyle: StyleProp<TextStyle> = { color: theme.colors.text };
   const logs = useSelector(({LOG}: RootState) => LOG.logs);
   const [filterLevel] = useState(LogLevel.None);
 
@@ -22,7 +25,11 @@ const SessionLogs: React.FC = () => {
     .filter(log => log.level <= filterLevel)
     .map(log => <LogDisplay log={log} key={log.timestamp} />);
 
-  return <>{filteredLogs}</>;
+  return (
+    <Text style={textStyle}>
+      {filteredLogs}
+    </Text>
+  );
 };
 
 export default SessionLogs;
