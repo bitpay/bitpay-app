e9e753c566c4e4a9edf88ea3c6935f48dc0c74e3
Chris Nguyen <cnguyen@bitpay.com>
2021-11-12 12:00:32 -0800

    Add deeplink prefix constant

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,6 +1,7 @@
 // GENERAL
 export const NETWORK = 'testnet';
 export const BASE_BITPAY_URL = 'https://test.bitpay.com';
+export const DEEPLINK_PREFIX = `bitpay`;
 
 // BWC
 export const BASE_BWS_URL = 'https://bws.bitpay.com/bws/api';
