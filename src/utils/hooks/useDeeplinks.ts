d84a5903350bfb762f05786fc7467872df0538a4
Chris Nguyen <cnguyen@bitpay.com>
2021-11-17 14:26:45 -0800

    Create hooks folder and create useLogger custom hook

--- src/utils/hooks/useDeeplinks.ts
+++ src/utils/hooks/useDeeplinks.ts
@@ -3,18 +3,19 @@ import {useEffect} from 'react';
 import {Linking} from 'react-native';
 import InAppBrowser from 'react-native-inappbrowser-reborn';
 import {useDispatch} from 'react-redux';
-import {DEEPLINK_PREFIX} from '../constants/config';
-import {BitpayIdScreens} from '../navigation/bitpay-id/BitpayIdStack';
-import {RootStackParamList, RootStacks} from '../Root';
-import {LogActions} from '../store/log';
+import {DEEPLINK_PREFIX} from '../../constants/config';
+import {BitpayIdScreens} from '../../navigation/bitpay-id/BitpayIdStack';
+import {RootStackParamList, RootStacks} from '../../Root';
+import { useLogger } from '.';
 
 export const useDeeplinks = () => {
   const dispatch = useDispatch();
+  const logger = useLogger();
 
   useEffect(() => {
     const urlEventHandler = ({url}: {url: string}) => {
       if (url && url.startsWith(`${DEEPLINK_PREFIX}://`)) {
-        dispatch(LogActions.info(`Deep link received: ${url}`));
+        logger.info(`Deep link received: ${url}`);
 
         try {
           // clicking a deeplink from the IAB in iOS doesn't auto-close the IAB, so do it manually
@@ -52,3 +53,5 @@ export const useDeeplinks = () => {
 
   return linkingOptions;
 };
+
+export default useDeeplinks;
