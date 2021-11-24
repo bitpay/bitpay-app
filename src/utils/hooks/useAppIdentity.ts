e381ec85098afeb465dcdec65aba88627b0315f2
Chris Nguyen <cnguyen@bitpay.com>
2021-11-23 17:32:27 -0800

    Simplify useAppIdentity hook

--- src/utils/hooks/useAppIdentity.ts
+++ src/utils/hooks/useAppIdentity.ts
@@ -1,30 +1,27 @@
 import BitAuth from 'bitauth';
 import {useDispatch, useSelector} from 'react-redux';
 import {useLogger} from '.';
-import {Network} from '../../constants';
 import {RootState} from '../../store';
 import {AppActions} from '../../store/app';
 import {AppIdentity} from '../../store/app/app.models';
 
-const useAppIdentity = (
-  network?: Network,
-): {identity?: AppIdentity; error?: any} => {
+const useAppIdentity = (): {identity?: AppIdentity; error?: any} => {
   const dispatch = useDispatch();
   const logger = useLogger();
-  const appNetwork = useSelector(({APP}: RootState) => APP.network);
-  const _network = network || (appNetwork as Network);
+  const { APP } = useSelector((root: RootState) => root);
 
-  let identity = useSelector(({APP}: RootState) => APP.identity[_network]);
+  let identity = APP.identity[APP.network];
 
   if (!identity || !Object.keys(identity).length || !identity.priv) {
     try {
+      logger.info('Generating new App Identity...');
       identity = BitAuth.generateSin();
     } catch (error) {
       logger.error('Error generating App Identity: ' + JSON.stringify(error));
       return {error};
     }
 
-    dispatch(AppActions.successGenerateAppIdentity(_network, identity));
+    dispatch(AppActions.successGenerateAppIdentity(APP.network, identity));
   }
 
   return {identity};
