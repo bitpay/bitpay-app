c7f8031acf203218ed6d1fcb52729140e027c948
lanchana <lanchanagupta@gmail.com>
2021-12-03 17:01:14 -0500

    update navbar styles

--- src/navigation/wallet/screens/ImportWallet.tsx
+++ src/navigation/wallet/screens/ImportWallet.tsx
@@ -3,8 +3,7 @@ import styled from 'styled-components/native';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import RecoveryPhrase from '../components/RecoveryPhrase';
 import FileOrText from '../components/FileOrText';
-import {Action} from '../../../styles/colors';
-
+import {Action, Black, White} from '../../../styles/colors';
 const ImportWallerContainer = styled.SafeAreaView`
   flex: 1;
   margin-top: 10px;
@@ -17,13 +16,27 @@ const ImportWallet = () => {
     <ImportWallerContainer>
       <Tab.Navigator
         screenOptions={{
+          tabBarIndicatorStyle: {
+            height: 48,
+            borderRadius: 50,
+            backgroundColor: Action,
+          },
+          tabBarActiveTintColor: White,
+          tabBarInactiveTintColor: Black,
+          tabBarItemStyle: {width: 145, height: 48},
           tabBarLabelStyle: {
             fontSize: 15,
             textTransform: 'capitalize',
-            color: Action,
+            fontWeight: '500',
           },
-          tabBarItemStyle: {width: 160},
-          tabBarIndicatorStyle: {backgroundColor: Action},
+          tabBarStyle: {
+            borderWidth: 5,
+            width: 300,
+            alignSelf: 'center',
+            borderRadius: 40,
+            backgroundColor: '#f2f2f2',
+            borderColor: '#f2f2f2',
+          },
         }}>
         <Tab.Screen name="Recovery Phrase" component={RecoveryPhrase} />
         <Tab.Screen name="File/Text" component={FileOrText} />
