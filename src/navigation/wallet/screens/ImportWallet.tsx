a805779d9296af8b0025223c1166262112f1510e
lanchana <lanchanagupta@gmail.com>
2021-12-14 13:58:21 -0500

    create a shared tabNavigation styles

--- src/navigation/wallet/screens/ImportWallet.tsx
+++ src/navigation/wallet/screens/ImportWallet.tsx
@@ -3,7 +3,8 @@ import styled from 'styled-components/native';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import RecoveryPhrase from '../components/RecoveryPhrase';
 import FileOrText from '../components/FileOrText';
-import {Action, Black, White} from '../../../styles/colors';
+import {ScreenOptions} from '../../../styles/tabNavigator';
+
 const ImportWallerContainer = styled.SafeAreaView`
   flex: 1;
   margin-top: 10px;
@@ -14,30 +15,7 @@ const ImportWallet = () => {
 
   return (
     <ImportWallerContainer>
-      <Tab.Navigator
-        screenOptions={{
-          tabBarIndicatorStyle: {
-            height: 48,
-            borderRadius: 50,
-            backgroundColor: Action,
-          },
-          tabBarActiveTintColor: White,
-          tabBarInactiveTintColor: Black,
-          tabBarItemStyle: {width: 145, height: 48},
-          tabBarLabelStyle: {
-            fontSize: 15,
-            textTransform: 'capitalize',
-            fontWeight: '500',
-          },
-          tabBarStyle: {
-            borderWidth: 5,
-            width: 300,
-            alignSelf: 'center',
-            borderRadius: 40,
-            backgroundColor: '#f2f2f2',
-            borderColor: '#f2f2f2',
-          },
-        }}>
+      <Tab.Navigator screenOptions={ScreenOptions(150)}>
         <Tab.Screen name="Recovery Phrase" component={RecoveryPhrase} />
         <Tab.Screen name="File/Text" component={FileOrText} />
       </Tab.Navigator>
