a9a2c3a707cf60cff80fb6de8320e55bc06853ce
lanchana <lanchanagupta@gmail.com>
2021-12-16 13:06:57 -0500

    add header icons placeholder

--- src/navigation/tabs/home/HomeStack.tsx
+++ src/navigation/tabs/home/HomeStack.tsx
@@ -5,7 +5,10 @@ import {
   baseScreenOptions,
 } from '../../../constants/NavigationOptions';
 import HomeRoot from './HomeRoot';
-import {View} from 'react-native';
+import styled from 'styled-components/native';
+import ProfileSvg from '../../../../assets/img/home/profile.svg';
+import ScanSvg from '../../../../assets/img/home/scan.svg';
+import {ScreenGutter} from '../../../components/styled/Containers';
 
 export type HomeStackParamList = {
   Root: undefined;
@@ -15,9 +18,28 @@ export enum HomeScreens {
   Root = 'Root',
 }
 
+const HeaderContainer = styled.View`
+  flex-direction: row;
+  margin: 0 ${ScreenGutter};
+`;
+
+const ScanImg = styled.View`
+  margin-right: ${ScreenGutter};
+`;
+
 const Home = createStackNavigator<HomeStackParamList>();
 
 const HomeStack = () => {
+  //  TODO: Update me
+  const HeaderRightComponent = (
+    <HeaderContainer>
+      <ScanImg>
+        <ScanSvg />
+      </ScanImg>
+      <ProfileSvg />
+    </HeaderContainer>
+  );
+
   return (
     <Home.Navigator
       initialRouteName={HomeScreens.Root}
@@ -31,8 +53,7 @@ const HomeStack = () => {
         options={{
           headerLeft: () => null,
           headerTitle: () => null,
-          // TODO: Add scanner and profile
-          headerRight: () => <View />,
+          headerRight: () => HeaderRightComponent,
         }}
       />
     </Home.Navigator>
