a015c83b92452f4e5625eab159f6b2d45870710b
lanchana <lanchanagupta@gmail.com>
2021-12-22 12:28:56 -0500

    move back button to camera screen

--- src/navigation/camera/CameraStack.tsx
+++ src/navigation/camera/CameraStack.tsx
@@ -1,8 +1,4 @@
 import React from 'react';
-import {
-  baseNavigatorOptions,
-  baseScreenOptions,
-} from '../../constants/NavigationOptions';
 import {createStackNavigator} from '@react-navigation/stack';
 import Camera from './screens/Camera';
 
@@ -18,16 +14,8 @@ const CameraNavigator = createStackNavigator<CameraStackParamList>();
 
 const CameraStack = () => {
   return (
-    <CameraNavigator.Navigator
-      screenOptions={{...baseNavigatorOptions, ...baseScreenOptions}}
-      initialRouteName={CameraScreens.Root}>
-      <CameraNavigator.Screen
-        options={{
-          gestureEnabled: false,
-        }}
-        name={CameraScreens.Root}
-        component={Camera}
-      />
+    <CameraNavigator.Navigator screenOptions={{headerShown: false}}>
+      <CameraNavigator.Screen name={CameraScreens.Root} component={Camera} />
     </CameraNavigator.Navigator>
   );
 };
--- src/navigation/camera/screens/Camera.tsx
+++ src/navigation/camera/screens/Camera.tsx
@@ -1,43 +1,50 @@
 import React from 'react';
-import {View} from 'react-native';
 import {RNCamera} from 'react-native-camera';
 import styled from 'styled-components/native';
+import {View} from 'react-native';
+import Back from '../../../components/back/Back';
+import {useNavigation} from '@react-navigation/native';
 
-const CameraContainer = styled.View`
-  width: 100%;
-  padding: 0 20px;
+const CameraContainer = styled.SafeAreaView`
+  flex: 1;
+`;
+
+const BackButton = styled.TouchableOpacity`
+  width: 41px;
+  margin-left: 10px;
 `;
 
 const Camera = () => {
+  const navigation = useNavigation();
+  const goBack = () => {
+    navigation.goBack();
+  };
   return (
     <CameraContainer>
-      <View style={{flex: 1, justifyContent: 'flex-end', alignItems: 'center'}}>
-        <RNCamera
-          style={{
-            flex: 0,
-            backgroundColor: '#fff',
-            borderRadius: 5,
-            padding: 15,
-            paddingHorizontal: 20,
-            alignSelf: 'center',
-            margin: 20,
-          }}
-          type={RNCamera.Constants.Type.back}
-          flashMode={RNCamera.Constants.FlashMode.auto}
-          captureAudio={false}
-          androidCameraPermissionOptions={{
-            title: 'Permission to use camera',
-            message: 'We need your permission to use your camera',
-            buttonPositive: 'Ok',
-            buttonNegative: 'Cancel',
-          }}
-          onGoogleVisionBarcodesDetected={({barcodes}) => {
-            if (barcodes[0] && barcodes[0].data) {
-              console.log(barcodes);
-            }
-          }}
-        />
-      </View>
+      <RNCamera
+        style={{
+          flex: 1,
+        }}
+        type={RNCamera.Constants.Type.back}
+        flashMode={RNCamera.Constants.FlashMode.auto}
+        captureAudio={false}
+        androidCameraPermissionOptions={{
+          title: 'Permission to use camera',
+          message: 'We need your permission to use your camera',
+          buttonPositive: 'Ok',
+          buttonNegative: 'Cancel',
+        }}
+        onGoogleVisionBarcodesDetected={({barcodes}) => {
+          if (barcodes[0] && barcodes[0].data) {
+            console.log(barcodes);
+          }
+        }}>
+        <View>
+          <BackButton onPress={goBack}>
+            <Back />
+          </BackButton>
+        </View>
+      </RNCamera>
     </CameraContainer>
   );
 };
