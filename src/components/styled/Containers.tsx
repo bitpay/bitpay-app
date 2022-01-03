ea63cf28c188c3091e04fbb8c271f6459e144668
lanchana <lanchanagupta@gmail.com>
2022-01-03 12:11:42 -0500

    fix typo

--- src/components/styled/Containers.tsx
+++ src/components/styled/Containers.tsx
@@ -1,6 +1,7 @@
 import styled, {css} from 'styled-components/native';
 import {Dimensions} from 'react-native';
-import {NeutralSlate} from '../../styles/colors';
+import {NeutralSlate, SlateDark} from '../../styles/colors';
+import {BaseText} from './Text';
 
 export const {height: HEIGHT, width: WIDTH} = Dimensions.get('window');
 
