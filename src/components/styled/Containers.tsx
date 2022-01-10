28104d22055bb1a6e73ca301f7cf056619a647b6
rastajpa <juanpabloabuin@gmail.com>
2022-01-10 08:38:19 -0800

    rebase master

--- src/components/styled/Containers.tsx
+++ src/components/styled/Containers.tsx
@@ -1,6 +1,6 @@
 import styled, {css} from 'styled-components/native';
 import {Dimensions} from 'react-native';
-import {LightBlack, NeutralSlate, SlateDark} from '../../styles/colors';
+import {NeutralSlate, SlateDark} from '../../styles/colors';
 import {BaseText} from './Text';
 
 export const {height: HEIGHT, width: WIDTH} = Dimensions.get('window');
