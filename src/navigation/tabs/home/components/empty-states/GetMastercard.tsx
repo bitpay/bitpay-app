46f43025130ca52d1229252f9e187659d4a6f00e
Chris Nguyen <cnguyen@bitpay.com>
2022-01-17 13:58:50 -0800

    Link to CardStack from Home and display balance if available.

--- src/navigation/tabs/home/components/empty-states/GetMastercard.tsx
+++ src/navigation/tabs/home/components/empty-states/GetMastercard.tsx
@@ -1,8 +1,13 @@
+import {useNavigation} from '@react-navigation/native';
 import React from 'react';
-import HomeCard from '../../../../../components/home-card/HomeCard';
-import MCLogo from '../../../../../../assets/img/logos/mc-logo.svg';
+import {useSelector} from 'react-redux';
 import styled from 'styled-components/native';
-import BitPayBBackgroundImg from '../../../../../../assets/img/logos/bitpay-b-background.svg';
+import BitPayBLogoBg from '../../../../../../assets/img/logos/bitpay-b-background.svg';
+import McLogo from '../../../../../../assets/img/logos/mc-logo.svg';
+import HomeCard from '../../../../../components/home-card/HomeCard';
+import {RootState} from '../../../../../store';
+import {Card} from '../../../../../store/card/card.models';
+import {format} from '../../../../../utils/currency';
 
 const HeaderImg = styled.View`
   width: 60px;
@@ -13,21 +18,42 @@ const HeaderImg = styled.View`
 
 const HeaderComponent = (
   <HeaderImg>
-    <MCLogo />
+    <McLogo />
   </HeaderImg>
 );
-const GetMastercard = () => {
-  const _onCTAPress = () => {
-    /** TODO: Redirect me */
+
+const GetMastercard: React.FC = () => {
+  const navigation = useNavigation();
+  const primaryCard = useSelector<RootState, Card | null>(({APP, CARD}) => {
+    const cards = CARD.cards[APP.network] || [];
+
+    return cards.find(c => c.provider === 'galileo') || null;
+  });
+  const primaryBalance = useSelector<RootState, number>(({CARD}) =>
+    primaryCard ? CARD.balances[primaryCard.id] : 0,
+  );
+
+  const body = primaryCard
+    ? {
+        title: 'BitPayCard',
+        value: format(primaryBalance, 'USD'),
+      }
+    : {
+        description: 'Get the BitPay prepaid Mastercard®',
+      };
+
+  const onCTAPress = () => {
+    navigation.navigate('Card', {
+      screen: 'Home',
+    });
   };
+
   return (
     <HomeCard
-      backgroundImg={() => <BitPayBBackgroundImg />}
+      backgroundImg={() => <BitPayBLogoBg />}
       header={HeaderComponent}
-      body={{description: 'Get the BitPay prepaid Mastercard®'}}
-      footer={{
-        onCTAPress: _onCTAPress,
-      }}
+      body={body}
+      onCTAPress={onCTAPress}
     />
   );
 };
