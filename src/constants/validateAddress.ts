95c23428f9deb44223aa0358a020343f5bfd024e
lanchana <lanchanagupta@gmail.com>
2022-01-18 18:01:45 -0500

    add address validation and update BWCProvider

---
+++ src/constants/validateAddress.ts
@@ -0,0 +1,26 @@
+import {BwcProvider} from '../lib/bwc';
+
+const BWC = BwcProvider.getInstance();
+
+export const ValidateAddress = (str: string, coin: string, network: string) => {
+  switch (coin) {
+    case 'btc':
+      const address = BWC.getBitcore().Address;
+      return !!address.isValid(str, network);
+    case 'bch':
+      const addressCash = BWC.getBitcoreCash().Address;
+      return !!addressCash.isValid(str, network);
+    case 'doge':
+      const addressDoge = BWC.getBitcoreDoge().Address;
+      return !!addressDoge.isValid(str, network);
+    case 'ltc':
+      const addressLtc = BWC.getBitcoreLtc().Address;
+      return !!addressLtc.isValid(str, network);
+    case 'eth':
+    case 'xrp':
+      const {Validation} = BWC.getCore();
+      return !!Validation.validateAddress(coin.toUpperCase(), network, str);
+    default:
+      return false;
+  }
+};
