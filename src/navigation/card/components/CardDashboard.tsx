1c2f1eb357421aa7740d2faa74f8d6d21ae07afd
Chris Nguyen <cnguyen@bitpay.com>
2022-01-18 10:54:55 -0800

    Add id screen param, scroll to slide if id passed in

--- src/navigation/card/components/CardDashboard.tsx
+++ src/navigation/card/components/CardDashboard.tsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useMemo} from 'react';
-import Carousel from 'react-native-snap-carousel';
+import {useRef, useState} from 'react';
+import Carousel, {Pagination} from 'react-native-snap-carousel';
 import {useDispatch, useSelector} from 'react-redux';
 import {WIDTH} from '../../../components/styled/Containers';
 import {RootState} from '../../../store';
@@ -8,7 +9,9 @@ import {Card} from '../../../store/card/card.models';
 import {VirtualDesignCurrency} from '../../../store/card/card.types';
 import CardOverviewSlide from './CardOverviewSlide';
 
-interface CardDashboardProps {}
+interface CardDashboardProps {
+  id: string | undefined | null;
+}
 
 const GroupEnabled = {
   firstView: false,
@@ -37,14 +40,66 @@ export class OverviewSlide {
   }
 }
 
-const CardDashboard: React.FC<CardDashboardProps> = () => {
+const createOverviewSlides = (cards: Card[]) => {
+  // sort galileo before firstView, then virtual before physical
+  const sortedCards = cards.sort((a, b) => {
+    if (a.provider === 'galileo' && b.provider === 'firstView') {
+      return -1;
+    }
+    if (a.provider === 'firstView' && b.provider === 'galileo') {
+      return 1;
+    }
+
+    if (a.cardType === 'virtual' && b.cardType === 'physical') {
+      return -1;
+    }
+    if (a.cardType === 'physical' && b.cardType === 'virtual') {
+      return 1;
+    }
+    return 0;
+  });
+
+  const slides = sortedCards.reduce((slideList, card) => {
+    if (!GroupEnabled[card.provider]) {
+      slideList.push(new OverviewSlide(card));
+
+      return slideList;
+    }
+
+    let slide = slideList.find(g => g.provider === card.provider);
+
+    if (slide) {
+      slide.add(card);
+    } else {
+      slideList.push(new OverviewSlide(card));
+    }
+
+    return slideList;
+  }, [] as OverviewSlide[]);
+
+  return slides;
+};
+
+const CardDashboard: React.FC<CardDashboardProps> = props => {
   const dispatch = useDispatch();
+  const {id} = props;
+  const carouselRef = useRef<Carousel<OverviewSlide>>(null);
   const cards = useSelector<RootState, Card[]>(
     ({APP, CARD}) => CARD.cards[APP.network],
   );
   const virtualDesignCurrency = useSelector<RootState, VirtualDesignCurrency>(
     ({CARD}) => CARD.virtualDesignCurrency,
   );
+  const memoizedSlides = useMemo(() => createOverviewSlides(cards), [cards]);
+  const [firstItem] = useState(() =>
+    id
+      ? Math.max(
+          0,
+          memoizedSlides.findIndex(s => s.cards.some(c => c.id === id)),
+        )
+      : 0,
+  );
+  const [activeIdx, setActiveIdx] = useState<number>(firstItem);
 
   // TODO: this is a placeholder just to populate with some data
   // TODO: build a graph query to initialize everything, then do ad-hoc updates
@@ -56,52 +111,14 @@ const CardDashboard: React.FC<CardDashboardProps> = () => {
     }
   }, [cards, dispatch]);
 
-  const memoizedSlides = useMemo(() => {
-    // sort galileo before firstView, then virtual before physical
-    const sortedCards = cards.sort((a, b) => {
-      if (a.provider === 'galileo' && b.provider === 'firstView') {
-        return -1;
-      }
-      if (a.provider === 'firstView' && b.provider === 'galileo') {
-        return 1;
-      }
-
-      if (a.cardType === 'virtual' && b.cardType === 'physical') {
-        return -1;
-      }
-      if (a.cardType === 'physical' && b.cardType === 'virtual') {
-        return 1;
-      }
-      return 0;
-    });
-
-    const slides = sortedCards.reduce((slideList, card) => {
-      if (!GroupEnabled[card.provider]) {
-        slideList.push(new OverviewSlide(card));
-
-        return slideList;
-      }
-
-      let slide = slideList.find(g => g.provider === card.provider);
-
-      if (slide) {
-        slide.add(card);
-      } else {
-        slideList.push(new OverviewSlide(card));
-      }
-
-      return slideList;
-    }, [] as OverviewSlide[]);
-
-    return slides;
-  }, [cards]);
-
   return (
     <>
       <Carousel<OverviewSlide>
+        ref={carouselRef}
         vertical={false}
         layout="default"
         activeSlideAlignment="center"
+        firstItem={firstItem}
         data={memoizedSlides}
         renderItem={({item}) => (
           <CardOverviewSlide
@@ -109,10 +126,20 @@ const CardDashboard: React.FC<CardDashboardProps> = () => {
             designCurrency={virtualDesignCurrency}
           />
         )}
+        onSnapToItem={setActiveIdx}
         itemWidth={300 + 20}
         sliderWidth={WIDTH}
         inactiveSlideScale={1}
         inactiveSlideOpacity={1}
+        containerCustomStyle={{
+          flexGrow: 0,
+        }}
+      />
+      <Pagination
+        dotsLength={memoizedSlides.length}
+        activeDotIndex={activeIdx}
+        carouselRef={carouselRef}
+        tappableDots={true}
       />
     </>
   );
--- src/navigation/card/screens/CardHome.tsx
+++ src/navigation/card/screens/CardHome.tsx
@@ -6,10 +6,15 @@ import {CardStackParamList} from '../CardStack';
 import CardDashboard from '../components/CardDashboard';
 import CardIntro from '../components/CardIntro';
 
-export type CardHomeScreenParamList = undefined;
+export type CardHomeScreenParamList =
+  | {
+      id: string | undefined | null;
+    }
+  | undefined;
 type CardHomeScreenProps = StackScreenProps<CardStackParamList, 'Home'>;
 
-const CardHome: React.FC<CardHomeScreenProps> = () => {
+const CardHome: React.FC<CardHomeScreenProps> = ({route}) => {
+  const {params} = route;
   const isDashboardEnabled = useSelector<RootState, boolean>(
     ({APP, BITPAY_ID, CARD}) => {
       const isPaired = !!BITPAY_ID.apiToken[APP.network];
@@ -25,7 +30,7 @@ const CardHome: React.FC<CardHomeScreenProps> = () => {
 
   return (
     <>
-      <DashboardOrIntro />
+      <DashboardOrIntro id={params?.id} />
     </>
   );
 };
