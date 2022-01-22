edda9f2c0f63ceac5dc186536f8f533d4d545aa6
johnathan White <jwhite@bitpay.com>
2022-01-22 10:54:14 -0500

    refactored import

--- src/store/wallet/effects/import/import.ts
+++ src/store/wallet/effects/import/import.ts
@@ -2,27 +2,22 @@ import {KeyMethods, KeyOptions, Wallet} from '../../wallet.models';
 import {Effect} from '../../../index';
 import {startOnGoingProcessModal} from '../../../app/app.effects';
 import {OnGoingProcessMessages} from '../../../../components/modal/ongoing-process/OngoingProcess';
-import {coinSupported} from '../../../../utils/helper-methods';
-import {Credentials} from 'bitcore-wallet-client/ts_build/lib/credentials';
 import {BwcProvider} from '../../../../lib/bwc';
 import merge from 'lodash.merge';
 import {buildWalletObj} from '../../utils/wallet';
 import {failedImport, successImport} from '../../wallet.actions';
 import {dismissOnGoingProcessModal} from '../../../app/app.actions';
+import {TokenOpts} from '../../../../constants/currencies';
 
 export const startImportMnemonic =
   (words: string, opts: Partial<KeyOptions>): Effect =>
   async dispatch => {
-    await dispatch(
-      startOnGoingProcessModal(OnGoingProcessMessages.IMPORTING_KEY),
-    );
+    await dispatch(startOnGoingProcessModal(OnGoingProcessMessages.IMPORTING));
     try {
       words = normalizeMnemonic(words);
       opts.words = words;
 
       const {key, wallets} = await serverAssistedImport(opts);
-
-      dispatch(dismissOnGoingProcessModal());
 
       dispatch(
         successImport({
@@ -41,10 +36,10 @@ export const startImportMnemonic =
       );
     } catch (e) {
       // TODO: Handle me
-      dispatch(dismissOnGoingProcessModal());
       dispatch(failedImport());
       console.error(e);
     }
+    dispatch(dismissOnGoingProcessModal());
   };
 
 export const normalizeMnemonic = (words: string): string => {
@@ -66,34 +61,50 @@ export const serverAssistedImport = async (
   opts: Partial<KeyOptions>,
 ): Promise<{key: KeyMethods; wallets: Wallet[]}> => {
   return new Promise(resolve => {
-    BwcProvider.API.serverAssistedImport(
-      opts,
-      {baseUrl: 'https://bws.bitpay.com/bws/api'},
-      // @ts-ignore
-      async (err, key, wallets) => {
-        if (err) {
-          //  TODO: Handle this
-        }
-        if (wallets.length === 0) {
-          //  TODO: Handle this - WALLET_DOES_NOT_EXIST
-        } else {
-          let customTokens: Array<Credentials & {tokens?: any}> = [];
-          wallets.forEach((w: any) => {
-            if (coinSupported(w.credentials.coin) && w.credentials.token) {
-              customTokens.push({
-                ...w.credentials.token,
-                ...{symbol: w.credentials.token.symbol.toLowerCase()},
-              });
-            }
-          });
-
-          if (customTokens && customTokens[0]) {
-            //  TODO: Create Custom Token
+    try {
+      BwcProvider.API.serverAssistedImport(
+        opts,
+        {baseUrl: 'https://bws.bitpay.com/bws/api'},
+        // @ts-ignore
+        async (err, key, wallets) => {
+          console.log('server assisted import starting');
+          if (err) {
+            //  TODO: Handle this
           }
+          if (wallets.length === 0) {
+            //  TODO: Handle this - WALLET_DOES_NOT_EXIST
+          } else {
+            console.log(wallets);
+            const tokens: Wallet[] = wallets.filter(
+              (wallet: Wallet) => !!wallet.credentials.token,
+            );
 
-          return resolve({key, wallets});
-        }
-      },
-    );
+            if (tokens && !!tokens.length) {
+              wallets = linkTokenToWallet(tokens, wallets);
+            }
+
+            return resolve({key, wallets});
+          }
+        },
+      );
+    } catch (err) {
+      console.log(err);
+    }
   });
+};
+
+const linkTokenToWallet = (tokens: Wallet[], wallets: Wallet[]) => {
+  tokens.forEach(token => {
+    const associatedWalletId = token.credentials.walletId.split('-0x')[0];
+    wallets = wallets.map((wallet: Wallet) => {
+      if (wallet.credentials.walletId === associatedWalletId) {
+        wallet.tokens = wallet.tokens || [];
+        const tokenOpt = TokenOpts[token.credentials.coin];
+        tokenOpt && wallet.tokens.push(tokenOpt);
+      }
+      return wallet;
+    });
+  });
+
+  return wallets;
 };
