25549437c3b08d09c5f2685b82546811ff65e32c
Chris Nguyen <cnguyen@bitpay.com>
2022-01-24 11:00:18 -0800

    Consolidate MastercardFront and VisaFront into a single component

--- src/navigation/card/components/CardOverviewSlide.tsx
+++ src/navigation/card/components/CardOverviewSlide.tsx
@@ -7,32 +7,16 @@ import {VirtualDesignCurrency} from '../../../store/card/card.types';
 import {format} from '../../../utils/currency';
 import {CardStackParamList} from '../CardStack';
 import {OverviewSlide} from './CardDashboard';
-import MastercardFront from './CardFront.Mastercard';
-import VisaFront from './CardFront.Visa';
+import CardFront from './CardFront';
 
 export interface CardOverviewSlideProps {
   slide: OverviewSlide;
   designCurrency: VirtualDesignCurrency;
 }
 
-export interface BrandFrontProps {
-  basic?: boolean;
-  balance: string;
-  nickname: string;
-  fiatSymbol: string;
-  fiat: string;
-  designCurrency: VirtualDesignCurrency;
-}
-
 const SlideContainer = styled.View`
   padding: 0 10px;
 `;
-
-const CARD_FRONT = {
-  default: MastercardFront,
-  galileo: MastercardFront,
-  firstView: VisaFront,
-};
 
 const CardOverviewSlide: React.FC<CardOverviewSlideProps> = ({
   designCurrency,
@@ -43,14 +27,14 @@ const CardOverviewSlide: React.FC<CardOverviewSlideProps> = ({
   const balance = useSelector<RootState, number>(
     ({CARD}) => CARD.balances[primaryCard.id],
   );
-  const CardFront =
-    (primaryCard.provider && CARD_FRONT[primaryCard.provider]) ||
-    CARD_FRONT.default;
+
   const formattedBalance = format(balance, primaryCard.currency.code);
 
   return (
     <SlideContainer onTouchEnd={() => navigation.navigate('Settings', {slide})}>
       <CardFront
+        brand={primaryCard.brand || 'Visa'}
+        provider={primaryCard.provider}
         balance={formattedBalance}
         nickname={primaryCard.nickname}
         fiat={primaryCard.currency.code}
