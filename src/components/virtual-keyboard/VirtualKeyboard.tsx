6a97c2a2a46f060a6597dc3c72646a62166467ff
lanchana <lanchanagupta@gmail.com>
2022-01-27 14:16:12 -0500

    add virtual keyboard component

---
+++ src/components/virtual-keyboard/VirtualKeyboard.tsx
@@ -0,0 +1,93 @@
+import React, {useEffect, useState} from 'react';
+import styled from 'styled-components/native';
+import {BaseText} from '../styled/Text';
+import DeleteSvg from '../../../assets/img/delete.svg';
+
+const KeyboardContainer = styled.View`
+  margin: 10px 0;
+`;
+
+const RowContainer = styled.View`
+  flex-direction: row;
+`;
+
+const CellContainer = styled.TouchableOpacity`
+  width: 33.333333%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CellText = styled(BaseText)`
+  font-size: 32.08px;
+  font-weight: 500;
+  color: ${({theme}) => theme.colors.text};
+  line-height: 65px;
+`;
+
+interface VirtualKeyboardConfig {
+  setValue: (value: string) => void;
+  resetValue: string | undefined;
+}
+
+const VirtualKeyboard = ({setValue, resetValue}: VirtualKeyboardConfig) => {
+  const [curVal, setCurVal] = useState('');
+
+  const onCellPress = (val: string) => {
+    let currentValue;
+    if (val === 'reset') {
+      currentValue = '';
+    } else if (val === 'backspace') {
+      currentValue = curVal.slice(0, -1);
+    } else {
+      currentValue = curVal + val;
+    }
+    setCurVal(currentValue);
+    setValue(currentValue);
+  };
+
+  useEffect(() => {
+    if (resetValue) {
+      onCellPress('reset');
+    }
+  }, [resetValue]);
+
+  const Cell = ({val}: {val: string}) => {
+    return (
+      <CellContainer onPress={() => onCellPress(val)}>
+        <CellText>{val}</CellText>
+      </CellContainer>
+    );
+  };
+
+  const Row = ({numArray}: {numArray: Array<string>}) => {
+    return (
+      <RowContainer>
+        {numArray ? numArray.map(val => <Cell val={val} key={val} />) : null}
+      </RowContainer>
+    );
+  };
+
+  return (
+    <KeyboardContainer>
+      <Row numArray={['1', '2', '3']} />
+      <Row numArray={['4', '5', '6']} />
+      <Row numArray={['7', '8', '9']} />
+
+      <RowContainer>
+        <CellContainer onPress={() => onCellPress('.')}>
+          <Cell val={'.'} />
+        </CellContainer>
+        <CellContainer onPress={() => onCellPress('0')}>
+          <Cell val={'0'} />
+        </CellContainer>
+        <CellContainer
+          onPress={() => onCellPress('backspace')}
+          onLongPress={() => onCellPress('reset')}>
+          <DeleteSvg />
+        </CellContainer>
+      </RowContainer>
+    </KeyboardContainer>
+  );
+};
+
+export default VirtualKeyboard;
