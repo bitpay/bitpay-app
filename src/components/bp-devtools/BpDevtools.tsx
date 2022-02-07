48e993443f191aac01d0182b6ebe3c333d63184c
Chris Nguyen <cnguyen@bitpay.com>
2022-02-07 13:36:46 -0800

    Add dev button to skip onboarding

--- src/components/bp-devtools/BpDevtools.tsx
+++ src/components/bp-devtools/BpDevtools.tsx
@@ -1,17 +1,36 @@
 import React from 'react';
-import {Appearance, ColorSchemeName, Switch, Text, View} from 'react-native';
+import {
+  Appearance,
+  ColorSchemeName,
+  Switch,
+  Text,
+  View,
+} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import styled from 'styled-components/native';
+import {navigationRef} from '../../Root';
 import {RootState} from '../../store';
 import {AppActions} from '../../store/app';
+import {Action, NeutralSlate, White} from '../../styles/colors';
 import haptic from '../haptic-feedback/haptic';
 
-const Container = styled.SafeAreaView`
+const Container = styled.View`
   align-items: center;
   border: 1px solid red;
   flex-direction: row;
   flex-grow: 1;
   padding: 10px;
+`;
+
+const DebugButton = styled.TouchableOpacity<{disabled?: boolean}>`
+  justify-content: center;
+  background-color: ${({disabled}) => (disabled ? NeutralSlate : Action)};
+  opacity: ${({disabled}) => (disabled ? 0.5 : 1)};
+  padding: 10px;
+`;
+
+const DebugButtonText = styled.Text`
+  color: ${White};
 `;
 
 const ThemeToggle = () => {
@@ -52,6 +71,31 @@ const LanguageToggle = () => {
   );
 };
 
+const SkipIntroButton = () => {
+  const dispatch = useDispatch();
+  const onboardingCompleted = useSelector<RootState, boolean>(
+    ({APP}) => APP.onboardingCompleted,
+  );
+
+  const onPress = () => {
+    if (onboardingCompleted) {
+      return;
+    }
+
+    dispatch(AppActions.setOnboardingCompleted());
+
+    navigationRef.navigate('Tabs', {screen: 'Home'});
+  };
+
+  return (
+    <Container>
+      <DebugButton disabled={onboardingCompleted} onPress={onPress}>
+        <DebugButtonText>Skip Onboarding</DebugButtonText>
+      </DebugButton>
+    </Container>
+  );
+};
+
 const BpDevtools = () => {
   return (
     <View
@@ -63,6 +107,7 @@ const BpDevtools = () => {
       }}>
       <ThemeToggle />
       <LanguageToggle />
+      <SkipIntroButton />
       {/* add more stuff */}
     </View>
   );
