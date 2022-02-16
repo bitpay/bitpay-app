dbb54b02e1590d5995896e0fa70bf2d629270a3e
Chris Nguyen <cnguyen@bitpay.com>
2022-02-16 12:35:45 -0800

    Remove kebab icon from card front

--- src/navigation/card/components/CardFront.tsx
+++ src/navigation/card/components/CardFront.tsx
@@ -253,28 +253,6 @@ const CardFront: React.FC<CardFrontProps> = props => {
                 </Svg.TSpan>
               </Svg.Text>
             </Svg.G>
-
-            <Svg.G
-              id="menu-icon"
-              transform="translate(271.000000, 20.000000)"
-              fill="#FFFFFF"
-              data-className="settings">
-              <Svg.Rect
-                id="Rectangle"
-                opacity="0.1"
-                x="0"
-                y="0"
-                width="33"
-                height="32"
-                rx="5"
-                data-class="settings"
-              />
-              <Svg.G id="Group-5" transform="translate(15.000000, 9.000000)">
-                <Svg.Circle id="Oval" cx="1.5" cy="1.5" r="1.5" />
-                <Svg.Circle id="Oval" cx="1.5" cy="7.5" r="1.5" />
-                <Svg.Circle id="Oval" cx="1.5" cy="13.5" r="1.5" />
-              </Svg.G>
-            </Svg.G>
           </>
         )}
 
