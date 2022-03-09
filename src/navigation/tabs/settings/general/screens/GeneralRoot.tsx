983022ee139f33034a86f004009b87c0b5242867
Johnathan White <jwhite@bitpay.com>
2022-03-08 22:08:25 -0500

    added remaining fields to general settings

--- src/navigation/tabs/settings/general/screens/GeneralRoot.tsx
+++ src/navigation/tabs/settings/general/screens/GeneralRoot.tsx
@@ -6,11 +6,15 @@ import {useSelector} from 'react-redux';
 import {RootState} from '../../../../../store';
 import {
   Hr,
+  Info,
+  InfoTriangle,
   Setting,
   SettingTitle,
 } from '../../../../../components/styled/Containers';
 import AngleRight from '../../../../../../assets/img/angle-right.svg';
 import {useTranslation} from 'react-i18next';
+import ToggleSwitch from '../../../../../components/toggle-switch/ToggleSwitch';
+import {InfoDescription} from '../../../../../components/styled/Text';
 
 const GeneralSettingsRoot: React.FC = () => {
   const navigation = useNavigation();
@@ -21,14 +25,7 @@ const GeneralSettingsRoot: React.FC = () => {
     <SettingsContainer>
       <Settings>
         <Hr />
-        <Setting
-          onPress={() =>
-            navigation.navigate('GeneralSettings', {screen: 'LanguageSettings'})
-          }>
-          <SettingTitle>{t('Language')}</SettingTitle>
-          <AngleRight />
-        </Setting>
-        <Hr />
+        {/*----------------------------------------------------------------------*/}
         <Setting
           onPress={() =>
             navigation.navigate('GeneralSettings', {screen: 'Theme'})
@@ -47,6 +44,7 @@ const GeneralSettingsRoot: React.FC = () => {
           </Button>
         </Setting>
         <Hr />
+        {/*----------------------------------------------------------------------*/}
         <Setting
           onPress={() =>
             navigation.navigate('GeneralSettings', {screen: 'CustomizeHome'})
@@ -55,6 +53,54 @@ const GeneralSettingsRoot: React.FC = () => {
           <AngleRight />
         </Setting>
         <Hr />
+        {/*----------------------------------------------------------------------*/}
+        <Setting
+          onPress={() =>
+            navigation.navigate('GeneralSettings', {screen: 'CustomizeHome'})
+          }>
+          <SettingTitle>{t('Show Portfolio')}</SettingTitle>
+          <ToggleSwitch onChange={() => null} isEnabled={false} />
+        </Setting>
+        <Hr />
+        {/*----------------------------------------------------------------------*/}
+        <Setting
+          onPress={
+            () => null // Todo
+          }>
+          <SettingTitle>{t('Display Currency')}</SettingTitle>
+          <AngleRight />
+        </Setting>
+        <Hr />
+        {/*----------------------------------------------------------------------*/}
+        <Setting
+          onPress={() =>
+            navigation.navigate('GeneralSettings', {screen: 'LanguageSettings'})
+          }>
+          <SettingTitle>{t('Language')}</SettingTitle>
+          <AngleRight />
+        </Setting>
+        <Hr />
+        {/*----------------------------------------------------------------------*/}
+        <Setting
+          onPress={
+            () => null // Todo
+          }>
+          <SettingTitle>{t('Reset All Settings')}</SettingTitle>
+        </Setting>
+        <Hr />
+        {/*----------------------------------------------------------------------*/}
+        <Setting>
+          <SettingTitle>{t('Use Unconfirmed Funds')}</SettingTitle>
+          <ToggleSwitch onChange={() => null} isEnabled={false} />
+        </Setting>
+        <Info>
+          <InfoTriangle />
+          <InfoDescription>
+            If enabled, wallets will also try to spend unconfirmed funds.
+            However, unconfirmed funds are not allow for spending with
+            merchants, BitPay Card loads, or BitPay in-app gift card purchases.
+          </InfoDescription>
+        </Info>
       </Settings>
     </SettingsContainer>
   );
