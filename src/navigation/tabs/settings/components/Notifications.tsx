7669040549d57801aaab88e3b666d018d00a2fce
lanchana <lanchanagupta@gmail.com>
2022-04-11 15:28:26 -0400

    add notifications placeholder

--- src/navigation/tabs/settings/components/Notifications.tsx
+++ src/navigation/tabs/settings/components/Notifications.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
-import {BaseText} from '../../../../components/styled/Text';
+import {Settings} from '../SettingsRoot';
+import {
+  Hr,
+  Setting,
+  SettingTitle,
+} from '../../../../components/styled/Containers';
+import Checkbox from '../../../../components/checkbox/Checkbox';
+import {View} from 'react-native';
+
 const Notifications = () => {
-  return <BaseText>TODO</BaseText>;
+  // TODO: Update me
+  const notificationsList = [
+    {title: 'Enable Push Notifications', checked: false},
+    {title: 'Confirmed Transactions', checked: false},
+    {title: 'Product Updates', checked: false},
+    {title: 'Offers & Promotions', checked: false},
+  ];
+  return (
+    <Settings>
+      {notificationsList.map(({title, checked}, i) => (
+        <View key={i}>
+          {i !== 0 ? <Hr /> : null}
+          <Setting>
+            <SettingTitle>{title}</SettingTitle>
+            <Checkbox onPress={() => {}} checked={checked} />
+          </Setting>
+        </View>
+      ))}
+    </Settings>
+  );
 };
 
 export default Notifications;
