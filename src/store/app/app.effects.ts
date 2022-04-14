0fdac50811c3afc2066cdcafdfc28d4bc9ba9595
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2022-04-14 16:37:16 -0300

    Add Coinbase to Home if is connected

--- src/store/app/app.effects.ts
+++ src/store/app/app.effects.ts
@@ -29,12 +29,13 @@ import {batch} from 'react-redux';
 import i18n from 'i18next';
 import {WalletActions} from '../wallet';
 import {coinbaseInitialize} from '../coinbase';
+import {COINBASE_ENV} from '../../api/coinbase/coinbase.constants';
 
 export const startAppInit = (): Effect => async (dispatch, getState) => {
   try {
     dispatch(LogActions.clear());
 
-    const {APP, BITPAY_ID, WALLET, CARD} = getState();
+    const {APP, BITPAY_ID, WALLET, CARD, COINBASE} = getState();
     const {network, pinLockActive, biometricLockActive, homeCarouselConfig} =
       APP;
 
@@ -111,7 +112,11 @@ export const startAppInit = (): Effect => async (dispatch, getState) => {
         id: 'bitpayCard',
         show: true,
       }));
-      dispatch(setHomeCarouselConfig([...keys, ...cards]));
+      const coinbase = {
+        id: 'coinbase',
+        show: !!COINBASE.token[COINBASE_ENV],
+      };
+      dispatch(setHomeCarouselConfig([...keys, ...cards, coinbase]));
     }
 
     await sleep(500);
