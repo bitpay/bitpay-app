d4a07b4dd8f9800920e6ca1140e3b6bc840100d5
Chris Nguyen <cnguyen@bitpay.com>
2022-04-26 12:00:19 -0700

    Replace lock icon in card lock overlay

--- src/navigation/card/components/LockCardOverlay.tsx
+++ src/navigation/card/components/LockCardOverlay.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled from 'styled-components/native';
-import LockSvg from '../../../../assets/img/lock.svg';
 import {CARD_HEIGHT, CARD_WIDTH} from '../../../constants/config.card';
 import {White} from '../../../styles/colors';
+import LockIcon from '../assets/settings/icon-lock.svg';
 import CardOverlayBackground from './CardOverlayBackground';
 
 const LockCardOverlayContainer = styled.View`
@@ -31,7 +31,7 @@ const LockCardOverlay: React.FC = () => {
       <CardOverlayBackground />
 
       <LockCardOverlayContent>
-        <LockSvg />
+        <LockIcon />
 
         <LockText>Card is locked</LockText>
       </LockCardOverlayContent>
