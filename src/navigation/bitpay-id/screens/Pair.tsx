46ee91a3f56514f5a72a63d5a7da8ec513b96054
Chris Nguyen <cnguyen@bitpay.com>
2022-05-11 09:00:53 -0700

    Remove card related code from BitPayId pairing screen

--- src/navigation/bitpay-id/screens/Pair.tsx
+++ src/navigation/bitpay-id/screens/Pair.tsx
@@ -1,36 +1,38 @@
-import {RouteProp, useRoute} from '@react-navigation/core';
 import {StackScreenProps} from '@react-navigation/stack';
 import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import styled from 'styled-components/native';
-import AlertBox from '../../../components/alert-box/AlertBox';
+import Spinner from '../../../components/spinner/Spinner';
 import {ScreenGutter} from '../../../components/styled/Containers';
-import {navigationRef} from '../../../Root';
 import {RootState} from '../../../store';
+import {AppActions} from '../../../store/app';
 import {BitPayIdActions, BitPayIdEffects} from '../../../store/bitpay-id';
 import {PairingBitPayIdStatus} from '../../../store/bitpay-id/bitpay-id.reducer';
-import {CardActions} from '../../../store/card';
-import {VirtualDesignCurrency} from '../../../store/card/card.types';
 import {BitpayIdStackParamList} from '../BitpayIdStack';
 
 export type PairScreenParamList =
   | {
       secret?: string;
       code?: string;
-      dashboardRedirect?: string;
-      vcd?: VirtualDesignCurrency;
     }
   | undefined;
 
-type PairScreenProps = StackScreenProps<BitpayIdStackParamList, 'Pair'>;
-
 const PairingContainer = styled.View`
   padding: ${ScreenGutter};
+  align-items: center;
 `;
 
-const Pair: React.FC<PairScreenProps> = ({navigation}) => {
+const SpinnerWrapper = styled.View`
+  margin-top: 20px;
+`;
+
+const Pair: React.FC<
+  StackScreenProps<BitpayIdStackParamList, 'Pair'>
+> = props => {
+  const {navigation, route} = props;
+  const {secret, code} = route.params || {};
+
   const dispatch = useDispatch();
-  const route = useRoute<RouteProp<BitpayIdStackParamList, 'Pair'>>();
   const pairingStatus = useSelector<RootState, PairingBitPayIdStatus>(
     ({BITPAY_ID}) => BITPAY_ID.pairingBitPayIdStatus,
   );
@@ -38,55 +40,67 @@ const Pair: React.FC<PairScreenProps> = ({navigation}) => {
     ({BITPAY_ID}) => BITPAY_ID.pairingBitPayIdError || '',
   );
 
-  const {secret, code} = route.params || {};
-  const virtualDesignCurrency = route.params?.vcd as
-    | VirtualDesignCurrency
-    | undefined;
-  const dashboardRedirect = Boolean(route.params?.dashboardRedirect);
-
   useEffect(() => {
     if (secret) {
       dispatch(BitPayIdEffects.startDeeplinkPairing(secret, code));
-    }
-  }, [dispatch, secret, code]);
-
-  useEffect(() => {
-    if (pairingStatus === 'success') {
-      if (dashboardRedirect) {
-        navigationRef.navigate('Tabs', {
-          screen: 'Card',
-          params: {
-            screen: 'CardHome',
-          },
-        });
-      } else {
+    } else {
+      const goToProfile = () => {
         navigation.replace('Profile');
-      }
+      };
 
       dispatch(
-        CardActions.virtualDesignCurrencyUpdated(
-          virtualDesignCurrency || 'bitpay-b',
-        ),
+        AppActions.showBottomNotificationModal({
+          title: 'Pairing failed',
+          message: 'No pairing data received.',
+          type: 'warning',
+          actions: [
+            {
+              text: 'OK',
+              action: goToProfile,
+            },
+          ],
+          enableBackdropDismiss: true,
+          onBackdropDismiss: goToProfile,
+        }),
       );
-      dispatch(BitPayIdActions.updatePairingBitPayIdStatus(null));
     }
-  }, [
-    navigation,
-    dispatch,
-    pairingStatus,
-    dashboardRedirect,
-    virtualDesignCurrency,
-  ]);
+  }, [dispatch, navigation, secret, code]);
+
+  useEffect(() => {
+    if (pairingStatus) {
+      const goToProfile = () => {
+        navigation.replace('Profile');
+        dispatch(BitPayIdActions.updatePairingBitPayIdStatus(null));
+      };
+
+      if (pairingStatus === 'success') {
+        goToProfile();
+      } else if (pairingStatus === 'failed') {
+        dispatch(
+          AppActions.showBottomNotificationModal({
+            type: 'error',
+            title: 'Pairing failed',
+            message: pairingError,
+            actions: [
+              {
+                primary: true,
+                action: goToProfile,
+                text: 'OK',
+              },
+            ],
+            enableBackdropDismiss: true,
+            onBackdropDismiss: goToProfile,
+          }),
+        );
+      }
+    }
+  }, [dispatch, navigation, pairingStatus, pairingError]);
 
   return (
     <PairingContainer>
-      {!secret ? (
-        <AlertBox type="warning">No pairing data received.</AlertBox>
-      ) : pairingStatus === 'failed' ? (
-        <AlertBox type="warning">
-          Pairing failed: {pairingError || 'An unexpected error occurred.'}
-        </AlertBox>
-      ) : null}
+      <SpinnerWrapper>
+        <Spinner size={60} />
+      </SpinnerWrapper>
     </PairingContainer>
   );
 };
