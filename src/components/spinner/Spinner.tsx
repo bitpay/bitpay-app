b0a498976fe40c2fefdce619b17394a8e4608b6b
Chris Nguyen <cnguyen@bitpay.com>
2022-05-11 13:14:28 -0700

    Update spinner with BitPayB icon and ring design per mocks

--- src/components/spinner/Spinner.tsx
+++ src/components/spinner/Spinner.tsx
@@ -6,7 +6,9 @@ import Animated, {
   withRepeat,
   withTiming,
 } from 'react-native-reanimated';
-import SpinnerIcon from './spinner-icon.svg';
+import styled from 'styled-components/native';
+import {Action, LightBlack} from '../../styles/colors';
+import {BitPayB} from './Spinner.Icons';
 
 const DEFAULT_SIZE = 30;
 
@@ -14,12 +16,34 @@ type SpinnerProps = {
   size?: number;
 };
 
+const SpinnerContainer = styled.View<{size: number}>`
+  height: ${({size}) => size}px;
+  justify-content: center;
+  position: relative;
+  width: ${({size}) => size}px;
+`;
+
+const SpinnerRing = styled(Animated.View)<{size: number}>`
+  border: 3px solid ${({theme}) => (theme.dark ? LightBlack : '#f6f7f8')};
+  border-top-color: ${Action};
+  border-radius: ${({size}) => size}px;
+  height: 100%;
+  width: 100%;
+`;
+
+const SpinnerIconContainer = styled.View`
+  align-self: center;
+  position: absolute;
+`;
+
 const Spinner = (props: SpinnerProps) => {
   const size = Math.max(props.size || DEFAULT_SIZE, 0);
+  const scale = 0.5;
+  const iconSize = scale * size;
 
-  const angle = useSharedValue(-90);
+  const angle = useSharedValue(-45);
   angle.value = withRepeat(
-    withTiming(630, {duration: 1500, easing: Easing.bezier(0.5, 0, 0.25, 1.2)}),
+    withTiming(675, {duration: 1500, easing: Easing.bezier(0.5, 0, 0.25, 1.2)}),
     -1,
     false,
   );
@@ -29,16 +53,13 @@ const Spinner = (props: SpinnerProps) => {
   }));
 
   return (
-    <Animated.View
-      style={[
-        {
-          height: size,
-          width: size,
-        },
-        spin,
-      ]}>
-      <SpinnerIcon height={'100%'} width={'100%'} />
-    </Animated.View>
+    <SpinnerContainer size={size}>
+      <SpinnerRing size={size} style={spin} />
+
+      <SpinnerIconContainer>
+        <BitPayB size={iconSize} />
+      </SpinnerIconContainer>
+    </SpinnerContainer>
   );
 };
 
