80d25a7eed8cde3c538b649c64d1e19ebc890542
Chris Nguyen <cnguyen@bitpay.com>
2022-05-20 13:12:45 -0700

    Add shop selectors for integrations

--- src/store/shop/shop.selectors.ts
+++ src/store/shop/shop.selectors.ts
@@ -1,7 +1,11 @@
 import {createSelector} from 'reselect';
 import {AppSelector} from '..';
 import {getCardConfigFromApiConfigMap} from '../../lib/gift-cards/gift-card';
-import {AvailableCardMap} from './shop.models';
+import {
+  AvailableCardMap,
+  CategoriesAndCurations,
+  DirectIntegrationMap,
+} from './shop.models';
 
 export const selectAvailableCardMap: AppSelector<AvailableCardMap> = ({
   SHOP,
@@ -12,4 +16,44 @@ export const selectAvailableCardMap: AppSelector<AvailableCardMap> = ({
 export const selectAvailableGiftCards = createSelector(
   [selectAvailableCardMap],
   availableCardMap => getCardConfigFromApiConfigMap(availableCardMap),
+);
+
+export const selectCategoriesAndCurations: AppSelector<
+  CategoriesAndCurations
+> = ({SHOP}) => {
+  return SHOP.categoriesAndCurations;
+};
+
+export const selectCategories = createSelector(
+  [selectCategoriesAndCurations],
+  categoriesAndCurations => {
+    return Object.values(categoriesAndCurations.categories);
+  },
+);
+
+export const selectIntegrationsMap: AppSelector<DirectIntegrationMap> = ({
+  SHOP,
+}) => {
+  return SHOP.integrations as DirectIntegrationMap;
+};
+
+export const selectIntegrations = createSelector(
+  [selectIntegrationsMap],
+  integrationsMap => {
+    return Object.values(integrationsMap);
+  },
+);
+
+export const selectCategoriesWithIntegrations = createSelector(
+  [selectCategories, selectIntegrations],
+  (categories, integrations) => {
+    return categories
+      .map(category => ({
+        ...category,
+        integrations: integrations.filter(integration =>
+          category.tags?.some((tag: string) => integration.tags.includes(tag)),
+        ),
+      }))
+      .filter(category => category.integrations.length);
+  },
 );
