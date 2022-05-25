0280bc7c65ca28b141b7ee8170ede37f9b2f5280
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2022-05-25 18:31:42 -0300

    Use environment variable to set API key automatically

--- scripts/braze-config.js
+++ scripts/braze-config.js
@@ -1,43 +1,42 @@
+#!/usr/bin/env node
+require('dotenv').config();
 const fs = require('fs');
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
 
-readline.question(
-  'Which platform? (ios, android or enter to cancel): ',
-  entry => {
-    if (!['ios', 'android'].includes(entry)) {
-      readline.close();
-      return;
-    }
-    readline.question('Enter Braze key: ', key => {
-      let content;
-      const platform = entry.toUpperCase();
-      if (platform === 'ANDROID') {
-        const file = `${__dirname}/../android/app/src/main/res/values/braze.xml`;
-        content = fs.readFileSync(file, 'utf8');
-        readline.question('Enter Braze endpoint: ', endpoint => {
-          readline.question('Enter fcm senderId: ', senderId => {
-            content = content.replace('BRAZE_API_KEY_REPLACE_ME', key);
-            content = content.replace(
-              'BRAZE_API_ENDPOINT_REPLACE_ME',
-              endpoint,
-            );
-            content = content.replace('BRAZE_SENDER_ID_REPLACE_ME', senderId);
-            fs.writeFileSync(file, content);
-            console.log(`Braze config successfully updated for ${platform}`);
-            readline.close();
-          });
-        });
-      } else {
-        const file = `${__dirname}/../ios/BitPayApp/AppDelegate.m`;
-        content = fs.readFileSync(file, 'utf8');
-        content = content.replace('BRAZE_API_KEY_REPLACE_ME', key);
-        fs.writeFileSync(file, content);
-        console.log(`Braze config successfully updated for ${platform}`);
-        readline.close();
-      }
-    });
-  },
+if (['development', 'production'].includes(process.env.NODE_ENV)) {
+  const envFile = `${__dirname}/../.env.${process.env.NODE_ENV}`;
+  const newEnvFile = `${__dirname}/../.env`;
+  fs.copyFileSync(envFile, newEnvFile);
+  console.log('.env file created!');
+} else {
+  console.log('Please, set NODE_ENV ["development" | "production"]');
+  return;
+}
+
+// Configure Braze for iOS
+const brazeConfigFileIOS = `${__dirname}/../ios/BitPayApp/AppDelegate.m`;
+let contentiOS = fs.readFileSync(brazeConfigFileIOS, 'utf8');
+contentiOS = contentiOS.replace(
+  'BRAZE_API_KEY_REPLACE_ME',
+  process.env.BRAZE_API_KEY_IOS,
 );
+fs.writeFileSync(brazeConfigFileIOS, contentiOS);
+
+// Configure Braze for Android
+const brazeConfigFileAndroid = `${__dirname}/../android/app/src/main/res/values/braze.xml`;
+let contentAndroid = fs.readFileSync(brazeConfigFileAndroid, 'utf8');
+
+contentAndroid = contentAndroid.replace(
+  'BRAZE_API_KEY_REPLACE_ME',
+  process.env.BRAZE_API_KEY_ANDROID,
+);
+contentAndroid = contentAndroid.replace(
+  'BRAZE_API_ENDPOINT_REPLACE_ME',
+  process.env.BRAZE_API_ENDPOINT,
+);
+contentAndroid = contentAndroid.replace(
+  'BRAZE_SENDER_ID_REPLACE_ME',
+  process.env.BRAZE_SENDER_ID,
+);
+fs.writeFileSync(brazeConfigFileAndroid, contentAndroid);
+
+console.log('Braze config successfully updated.');
