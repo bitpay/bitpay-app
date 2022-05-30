68b67bb949a19ea04a06015a3b48935deecaade8
Chris Nguyen <cnguyen@bitpay.com>
2022-05-30 12:57:21 -0700

    Update Onboarding CreateKey to support smaller screens / display scaling

--- src/navigation/onboarding/screens/CreateKey.tsx
+++ src/navigation/onboarding/screens/CreateKey.tsx
@@ -1,6 +1,11 @@
+import {StackScreenProps} from '@react-navigation/stack';
 import React, {useLayoutEffect} from 'react';
+import {ScrollView} from 'react-native';
+import {useAndroidBackHandler} from 'react-navigation-backhandler';
 import styled from 'styled-components/native';
-import {H3, Paragraph, TextAlign} from '../../../components/styled/Text';
+import Button from '../../../components/button/Button';
+import {OnboardingImage} from '../components/Containers';
+import haptic from '../../../components/haptic-feedback/haptic';
 import {
   ActionContainer,
   CtaContainer,
@@ -9,20 +14,19 @@ import {
   TextContainer,
   TitleContainer,
 } from '../../../components/styled/Containers';
-import Button from '../../../components/button/Button';
-import {useNavigation} from '@react-navigation/native';
-import {useAndroidBackHandler} from 'react-navigation-backhandler';
-import {OnboardingImage} from '../components/Containers';
-import haptic from '../../../components/haptic-feedback/haptic';
+import {H3, Paragraph, TextAlign} from '../../../components/styled/Text';
+import {OnboardingStackParamList} from '../OnboardingStack';
 
 const CreateKeyContainer = styled.SafeAreaView`
   flex: 1;
-  align-items: center;
+  align-items: stretch;
 `;
 const KeyImage = require('../../../../assets/img/onboarding/create-wallet.png');
 
-const CreateOrImportKey = () => {
-  const navigation = useNavigation();
+const CreateOrImportKey: React.VFC<
+  StackScreenProps<OnboardingStackParamList, 'CreateKey'>
+> = ({navigation}) => {
+  useAndroidBackHandler(() => true);
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -34,11 +38,8 @@ const CreateOrImportKey = () => {
             buttonType={'pill'}
             onPress={() => {
               haptic('impactLight');
-              navigation.navigate('Onboarding', {
-                screen: 'TermsOfUse',
-                params: {
-                  context: 'TOUOnly',
-                },
+              navigation.navigate('TermsOfUse', {
+                context: 'TOUOnly',
               });
             }}>
             Skip
@@ -48,53 +49,57 @@ const CreateOrImportKey = () => {
     });
   }, [navigation]);
 
-  useAndroidBackHandler(() => true);
-
   return (
     <CreateKeyContainer>
-      <ImageContainer>
-        <OnboardingImage style={{width: 155, height: 247}} source={KeyImage} />
-      </ImageContainer>
-      <TitleContainer>
-        <TextAlign align={'center'}>
-          <H3>Create a key or import an existing key</H3>
-        </TextAlign>
-      </TitleContainer>
-      <TextContainer>
-        <TextAlign align={'center'}>
-          <Paragraph>
-            Store your assets safely and securely with BitPay's non-custodial
-            app. Reminder: you own your keys, so be sure to have a pen and paper
-            handy to write down your 12 words.
-          </Paragraph>
-        </TextAlign>
-      </TextContainer>
-      <CtaContainer>
-        <ActionContainer>
-          <Button
-            buttonStyle={'primary'}
-            onPress={() =>
-              navigation.navigate('Onboarding', {
-                screen: 'CurrencySelection',
-                params: {context: 'onboarding'},
-              })
-            }>
-            Create a Key
-          </Button>
-        </ActionContainer>
-        <ActionContainer>
-          <Button
-            buttonStyle={'secondary'}
-            onPress={() =>
-              navigation.navigate('Onboarding', {
-                screen: 'Import',
-                params: {context: 'onboarding'},
-              })
-            }>
-            I already have a Key
-          </Button>
-        </ActionContainer>
-      </CtaContainer>
+      <ScrollView
+        contentContainerStyle={{
+          alignItems: 'center',
+        }}>
+        <ImageContainer>
+          <OnboardingImage
+            style={{width: 155, height: 247}}
+            source={KeyImage}
+          />
+        </ImageContainer>
+        <TitleContainer>
+          <TextAlign align={'center'}>
+            <H3>Create a key or import an existing key</H3>
+          </TextAlign>
+        </TitleContainer>
+        <TextContainer>
+          <TextAlign align={'center'}>
+            <Paragraph>
+              Store your assets safely and securely with BitPay's non-custodial
+              app. Reminder: you own your keys, so be sure to have a pen and
+              paper handy to write down your 12 words.
+            </Paragraph>
+          </TextAlign>
+        </TextContainer>
+        <CtaContainer>
+          <ActionContainer>
+            <Button
+              buttonStyle={'primary'}
+              onPress={() =>
+                navigation.navigate('CurrencySelection', {
+                  context: 'onboarding',
+                })
+              }>
+              Create a Key
+            </Button>
+          </ActionContainer>
+          <ActionContainer>
+            <Button
+              buttonStyle={'secondary'}
+              onPress={() =>
+                navigation.navigate('Import', {
+                  context: 'onboarding',
+                })
+              }>
+              I already have a Key
+            </Button>
+          </ActionContainer>
+        </CtaContainer>
+      </ScrollView>
     </CreateKeyContainer>
   );
 };
