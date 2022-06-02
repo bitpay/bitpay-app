3fd8edbfacfdf7117e955d80b8e812b6da1ff60e
Chris Nguyen <cnguyen@bitpay.com>
2022-06-02 09:20:42 -0700

    Remove reanimated from intro screens
    Seems to be one of the culprits causing a crash after doing a migration when navigating from the Intro stack to the Tabs stack on Android.

--- src/navigation/intro/screens/Start.tsx
+++ src/navigation/intro/screens/Start.tsx
@@ -1,16 +1,25 @@
+import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
-import styled from 'styled-components/native';
+import styled, {useTheme} from 'styled-components/native';
 import FeatureCard from '../../../components/feature-card/FeatureCard';
-import {useNavigation, useTheme} from '@react-navigation/native';
+import {IntroStackParamList} from '../IntroStack';
+
 const lightImage = require('../../../../assets/img/intro/light/whats-new.png');
 const darkImage = require('../../../../assets/img/intro/dark/whats-new.png');
+
 const IntroStartContainer = styled.View`
   flex: 1;
 `;
 
-const Start = () => {
-  const navigation = useNavigation();
+type IntroStartScreenProps = StackScreenProps<IntroStackParamList, 'Start'>;
+
+const Start: React.VFC<IntroStartScreenProps> = ({navigation}) => {
   const theme = useTheme();
+
+  const onNext = () => {
+    navigation.navigate('WhatsNew');
+  };
+
   return (
     <IntroStartContainer>
       <FeatureCard
@@ -20,7 +29,7 @@ const Start = () => {
           'Your home tab is now your launchpad. View all your keys and check out new offerings from BitPay.'
         }
         ctaText={'Check it out'}
-        cta={() => navigation.navigate('Intro', {screen: 'WhatsNew'})}
+        cta={onNext}
       />
     </IntroStartContainer>
   );
