da0c43ea3b31584884e55bb9f5c56dccad801c96
Gabriel <gabrielbazan89@gmail.com>
2022-06-15 11:48:26 -0300

    [FIX] home and key view pull to refresh

--- src/store/wallet/effects/import/import.ts
+++ src/store/wallet/effects/import/import.ts
@@ -189,7 +189,7 @@ export const startMigration =
 
         // update store with token rates from coin gecko and update balances
         await dispatch(startGetRates({force: true}));
-        await dispatch(startUpdateAllKeyAndWalletStatus());
+        await dispatch(startUpdateAllKeyAndWalletStatus({force: true}));
       } catch (err) {
         dispatch(LogActions.info('Failed to migrate keys'));
         // flag for showing error modal
