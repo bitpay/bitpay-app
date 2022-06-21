478bfac33abd934f16d03100f081221400776ec0
lanchana shivananda <lshivananda@bitpay.com>
2022-06-20 22:19:54 -0400

    update card highlight translations

--- src/navigation/card/components/CardIntroHighlights.tsx
+++ src/navigation/card/components/CardIntroHighlights.tsx
@@ -11,6 +11,7 @@ import {ScreenGutter} from '../../../components/styled/Containers';
 import {BaseText, Exp, H4} from '../../../components/styled/Text';
 import {URL} from '../../../constants';
 import {Black, LuckySevens} from '../../../styles/colors';
+import {t} from 'i18next';
 
 interface CardHighlight {
   icon: React.FC<SvgProps>;
@@ -43,85 +44,92 @@ const SubText = styled(BaseText)`
   color: ${({theme: {dark}}) => (dark ? LuckySevens : Black)};
 `;
 /* add translation */
-const CARD_HIGHLIGHTS: CardHighlight[] = [
-  {
-    icon: InstantReloadsIcon,
-    title: (
-      <HighlightTitle>
-        Instant reloads
-        <Exp i={1} />
-      </HighlightTitle>
-    ),
-    description: (
-      <SubText>
-        Reload your balance with no conversion fees. Powered by our{' '}
-        <A href={URL.EXCHANGE_RATES}>competitive exchange rates</A>.
-      </SubText>
-    ),
-  },
-  {
-    icon: FlexibilityIcon,
-    title: <HighlightTitle>Flexibility</HighlightTitle>,
-    description: (
-      <SubText>
-        View your balance, request a new PIN and reload instantly all within the
-        BitPay App.
-      </SubText>
-    ),
-  },
-  {
-    icon: EnhancedSecurityIcon,
-    title: <HighlightTitle>Enhanced security</HighlightTitle>,
-    description: (
-      <SubText>
-        Includes EMV chip and options to lock your card and control how you
-        spend.
-      </SubText>
-    ),
-  },
-  {
-    icon: WorldwideIcon,
-    title: <HighlightTitle>Worldwide</HighlightTitle>,
-    description: (
-      <SubText>
-        Ready to use in millions of locations around the world with contactless
-        payment, PIN, Google Pay, Apple Pay, or by simply withdrawing cash from
-        any compatible ATM
-        <Exp i={2} />.
-      </SubText>
-    ),
-  },
-  {
-    icon: EarnCashIcon,
-    title: <HighlightTitle>Earn cash</HighlightTitle>,
-    description: (
-      <SubText>
-        Refer friends and you'll each receive $10 when they load their first
-        $100.
-      </SubText>
-    ),
-  },
-];
+const CARD_HIGHLIGHTS = (): CardHighlight[] => {
+  return [
+    {
+      icon: InstantReloadsIcon,
+      title: (
+        <HighlightTitle>
+          {t('Instant reloads')}
+          <Exp i={1} />
+        </HighlightTitle>
+      ),
+      description: (
+        <SubText>
+          {t('Reload your balance with no conversion fees. Powered by our')}{' '}
+          <A href={URL.EXCHANGE_RATES}>{t('competitive exchange rates')}</A>.
+        </SubText>
+      ),
+    },
+    {
+      icon: FlexibilityIcon,
+      title: <HighlightTitle>{t('Flexibility')}</HighlightTitle>,
+      description: (
+        <SubText>
+          {t(
+            'View your balance, request a new PIN and reload instantly all within the BitPay App.',
+          )}
+        </SubText>
+      ),
+    },
+    {
+      icon: EnhancedSecurityIcon,
+      title: <HighlightTitle>{t('Enhanced security')}</HighlightTitle>,
+      description: (
+        <SubText>
+          {t(
+            'Includes EMV chip and options to lock your card and control how you spend.',
+          )}
+        </SubText>
+      ),
+    },
+    {
+      icon: WorldwideIcon,
+      title: <HighlightTitle>{t('Worldwide')}</HighlightTitle>,
+      description: (
+        <SubText>
+          {t(
+            'Ready to use in millions of locations around the world with contactless payment, PIN, Google Pay, Apple Pay, or by simply withdrawing cash from any compatible ATM',
+          )}
+          <Exp i={2} />.
+        </SubText>
+      ),
+    },
+    {
+      icon: EarnCashIcon,
+      title: <HighlightTitle>{t('Earn cash')}</HighlightTitle>,
+      description: (
+        <SubText>
+          {t(
+            "Refer friends and you'll each receive $10 when they load their first $100.",
+          )}
+        </SubText>
+      ),
+    },
+  ];
+};
 
-const CardHighlights = (
-  <>
-    {CARD_HIGHLIGHTS.map((highlight, idx) => {
-      const Icon = highlight.icon;
+const CardHighlights = () => {
+  return (
+    <>
+      {CARD_HIGHLIGHTS().map((highlight, idx) => {
+        const Icon = highlight.icon;
 
-      return (
-        <Highlight key={idx}>
-          <HighlightIconContainer>
-            <Icon />
-          </HighlightIconContainer>
+        return (
+          <Highlight key={idx}>
+            <HighlightIconContainer>
+              <Icon />
+            </HighlightIconContainer>
 
-          <HighlightContentContainer>
-            {highlight.title}
-            {highlight.description}
-          </HighlightContentContainer>
-        </Highlight>
-      );
-    })}
-  </>
-);
+            <HighlightContentContainer>
+              {highlight.title}
+              {highlight.description}
+            </HighlightContentContainer>
+          </Highlight>
+        );
+      })}
+    </>
+  );
+};
 
 export default CardHighlights;
