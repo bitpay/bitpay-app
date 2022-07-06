2565158b98a7230068fdd29aaa7224a055ac0009
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2022-07-05 22:59:51 -0300

    For some reason if function does not have return new Promise, it does not return error

--- src/store/wallet/effects/init/init.ts
+++ src/store/wallet/effects/init/init.ts
@@ -5,24 +5,26 @@ import {startGetTokenOptions} from '../currencies/currencies';
 import {startUpdateAllKeyAndWalletStatus} from '../status/status';
 
 export const startWalletStoreInit =
-  (): Effect<Promise<void>> => async (dispatch, getState: () => RootState) => {
-    try {
-      const {WALLET, APP} = getState();
-      const defaultAltCurrencyIsoCode = APP.defaultAltCurrency.isoCode;
+  (): Effect<Promise<void>> => (dispatch, getState: () => RootState) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const {WALLET, APP} = getState();
+        const defaultAltCurrencyIsoCode = APP.defaultAltCurrency.isoCode;
 
-      // both needed for startUpdateAllKeyAndWalletStatus
-      await dispatch(startGetTokenOptions()); // needed for getRates. Get more recent 1inch tokens list
-      await dispatch(startGetRates({init: true})); // populate rates and alternative currency list
+        // both needed for startUpdateAllKeyAndWalletStatus
+        await dispatch(startGetTokenOptions()); // needed for getRates. Get more recent 1inch tokens list
+        await dispatch(startGetRates({init: true})); // populate rates and alternative currency list
 
-      if (Object.keys(WALLET.keys).length) {
-        dispatch(startUpdateAllKeyAndWalletStatus());
+        if (Object.keys(WALLET.keys).length) {
+          dispatch(startUpdateAllKeyAndWalletStatus());
+        }
+
+        dispatch(getPriceHistory(defaultAltCurrencyIsoCode));
+        dispatch(WalletActions.successWalletStoreInit());
+        return resolve();
+      } catch (e) {
+        dispatch(WalletActions.failedWalletStoreInit());
+        return reject(e);
       }
-
-      dispatch(getPriceHistory(defaultAltCurrencyIsoCode));
-      dispatch(WalletActions.successWalletStoreInit());
-      Promise.resolve();
-    } catch (e) {
-      dispatch(WalletActions.failedWalletStoreInit());
-      Promise.reject(e);
-    }
+    });
   };
