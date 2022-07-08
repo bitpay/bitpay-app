db8cc2dc277a636ad36a2e8d4e65f88f58b3b0bf
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2022-07-08 11:01:29 -0300

    Ref: adds button to send session logs by email

--- src/navigation/tabs/settings/about/screens/SessionLog.tsx
+++ src/navigation/tabs/settings/about/screens/SessionLog.tsx
@@ -1,11 +1,15 @@
 import {StackNavigationProp} from '@react-navigation/stack';
 import React, {useState} from 'react';
-import {StyleProp, Text, TextStyle} from 'react-native';
+import Mailer from 'react-native-mail';
+import {Alert} from 'react-native';
 import {useSelector} from 'react-redux';
-import styled, {useTheme} from 'styled-components/native';
+import styled from 'styled-components/native';
 import {RootState} from '../../../../../store';
 import {LogLevel} from '../../../../../store/log/log.models';
 import {AboutStackParamList} from '../AboutStack';
+import Button from '../../../../../components/button/Button';
+import {SlateDark} from '../../../../../styles/colors';
+import {BaseText} from '../../../../../components/styled/Text';
 
 export interface SessionLogsParamList {}
 
@@ -14,28 +18,88 @@ type SessionLogsScreenProps = StackNavigationProp<
   'SessionLogs'
 >;
 
+const LogsContainer = styled.SafeAreaView`
+  flex: 1;
+`;
+
 const ScrollView = styled.ScrollView`
   margin: 20px 15px;
 `;
 
+const ButtonContainer = styled.View`
+  padding: 10px 15px;
+`;
+
+const Logs = styled(BaseText)`
+  font-size: 14px;
+  color: ${({theme: {dark}}) => (dark ? '#E1E4E7' : SlateDark)};
+`;
+
 const SessionLogs: React.FC<SessionLogsScreenProps> = () => {
-  const theme = useTheme();
-  const textStyle: StyleProp<TextStyle> = {color: theme.colors.text};
   const logs = useSelector(({LOG}: RootState) => LOG.logs);
   const [filterLevel] = useState(LogLevel.None);
+
+  let logStr: string =
+    'Session Logs.\nBe careful, this could contain sensitive private data\n\n';
+  logStr += '\n\n';
 
   const filteredLogs = logs
     .filter(log => log.level <= filterLevel)
     .map(log => {
       const formattedLevel = LogLevel[log.level].toLowerCase();
 
-      return `[${log.timestamp}] [${formattedLevel}] ${log.message}\n`;
+      const output = `[${formattedLevel}] ${log.message}\n`;
+      logStr += output;
+      return `[${log.timestamp}] ${output}`;
     });
 
+  const handleEmail = (data: string) => {
+    Mailer.mail(
+      {
+        subject: 'BitPay Log',
+        body: data,
+        isHTML: false,
+      },
+      (error, event) => {
+        Alert.alert(
+          error,
+          event,
+          [
+            {
+              text: 'Ok',
+              onPress: () => console.log('OK: Email Error Response'),
+            },
+            {
+              text: 'Cancel',
+              onPress: () => console.log('CANCEL: Email Error Response'),
+            },
+          ],
+          {cancelable: true},
+        );
+      },
+    );
+  };
+
+  const showDisclaimer = (data: string) => {
+    Alert.alert(
+      'Warning',
+      'Be careful, this could contain sensitive private data.',
+      [{text: 'Continue', onPress: () => handleEmail(data)}, {text: 'Cancel'}],
+      {cancelable: true},
+    );
+  };
+
   return (
-    <ScrollView>
-      <Text style={textStyle}>{filteredLogs}</Text>
-    </ScrollView>
+    <LogsContainer>
+      <ScrollView>
+        <Logs>{filteredLogs}</Logs>
+      </ScrollView>
+      <ButtonContainer>
+        <Button onPress={() => showDisclaimer(logStr)}>
+          Send Logs By Email
+        </Button>
+      </ButtonContainer>
+    </LogsContainer>
   );
 };
 
