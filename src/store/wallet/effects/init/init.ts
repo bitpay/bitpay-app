f4bd5588ee63f2dedb180ef9771e4923c843e221
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2022-07-21 11:44:59 -0300

    Removes unnecessary Promise. Avoid duplicated Network Error message

--- src/store/wallet/effects/init/init.ts
+++ src/store/wallet/effects/init/init.ts
@@ -6,42 +6,31 @@ import {startUpdateAllKeyAndWalletStatus} from '../status/status';
 import {LogActions} from '../../../log';
 
 export const startWalletStoreInit =
-  (): Effect<Promise<void>> => (dispatch, getState: () => RootState) => {
-    return new Promise(async (resolve, reject) => {
-      dispatch(LogActions.info('starting [startWalletStoreInit]'));
-      try {
-        const {WALLET, APP} = getState();
-        const defaultAltCurrencyIsoCode = APP.defaultAltCurrency.isoCode;
+  (): Effect<Promise<void>> => async (dispatch, getState: () => RootState) => {
+    dispatch(LogActions.info('starting [startWalletStoreInit]'));
+    try {
+      const {WALLET, APP} = getState();
+      const defaultAltCurrencyIsoCode = APP.defaultAltCurrency.isoCode;
 
-        // both needed for startUpdateAllKeyAndWalletStatus
-        await dispatch(startGetTokenOptions()); // needed for getRates. Get more recent 1inch tokens list
-        await dispatch(startGetRates({init: true})); // populate rates and alternative currency list
+      // both needed for startUpdateAllKeyAndWalletStatus
+      await dispatch(startGetTokenOptions()); // needed for getRates. Get more recent 1inch tokens list
+      await dispatch(startGetRates({init: true})); // populate rates and alternative currency list
 
-        if (Object.keys(WALLET.keys).length) {
-          dispatch(startUpdateAllKeyAndWalletStatus());
-        }
-
-        dispatch(getPriceHistory(defaultAltCurrencyIsoCode));
-        dispatch(WalletActions.successWalletStoreInit());
-        dispatch(LogActions.info('success [startWalletStoreInit]'));
-        return resolve();
-      } catch (e) {
-        let errorStr;
-        if (e instanceof Error) {
-          errorStr = e.message;
-        } else {
-          errorStr = JSON.stringify(e);
-        }
-        if (errorStr === 'Network Error') {
-          dispatch(LogActions.warn(`[startWalletStoreInit] ${errorStr}`));
-          return resolve();
-        } else {
-          dispatch(WalletActions.failedWalletStoreInit());
-          dispatch(
-            LogActions.error(`failed [startWalletStoreInit]: ${errorStr}`),
-          );
-          return reject(e);
-        }
+      if (Object.keys(WALLET.keys).length) {
+        dispatch(startUpdateAllKeyAndWalletStatus());
       }
-    });
+
+      dispatch(getPriceHistory(defaultAltCurrencyIsoCode));
+      dispatch(WalletActions.successWalletStoreInit());
+      dispatch(LogActions.info('success [startWalletStoreInit]'));
+    } catch (e) {
+      let errorStr;
+      if (e instanceof Error) {
+        errorStr = e.message;
+      } else {
+        errorStr = JSON.stringify(e);
+      }
+      dispatch(WalletActions.failedWalletStoreInit());
+      dispatch(LogActions.error(`failed [startWalletStoreInit]: ${errorStr}`));
+    }
   };
