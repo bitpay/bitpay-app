bee7206871bd8eea9e023bdcb296f55c8cf44f24
Chris Nguyen <cnguyen@bitpay.com>
2022-07-29 15:38:33 -0700

    Add styling to new CurrencySelection UI

--- src/components/list/CurrencySelectionRow.tsx
+++ src/components/list/CurrencySelectionRow.tsx
@@ -1,19 +1,21 @@
 import React, {memo} from 'react';
 import {useTranslation} from 'react-i18next';
-import {View} from 'react-native';
 import styled from 'styled-components/native';
+import ArrowDownRightIcon from '../../../assets/img/arrow-down-right.svg';
 import {IS_ANDROID} from '../../constants';
 import {SupportedCurrencyOption} from '../../constants/SupportedCurrencyOptions';
+import {
+  LightBlack,
+  LuckySevens,
+  Slate10,
+  Slate30,
+  SlateDark,
+} from '../../styles/colors';
 import Checkbox from '../checkbox/Checkbox';
 import {CurrencyImage} from '../currency-image/CurrencyImage';
 import haptic from '../haptic-feedback/haptic';
-import {
-  ActiveOpacity,
-  CurrencyColumn,
-  CurrencyImageContainer,
-  RowContainer,
-} from '../styled/Containers';
-import {H5, ListItemSubText} from '../styled/Text';
+import {ScreenGutter} from '../styled/Containers';
+import {BaseText, H6, H7} from '../styled/Text';
 
 export type CurrencySelectionItem = Pick<
   SupportedCurrencyOption,
@@ -42,9 +44,63 @@ export type CurrencySelectionRowProps = {
   ) => any;
 };
 
-const CheckBoxContainer = styled.View`
+const CurrencySelectionRowContainer = styled.View`
+  border: 1px solid ${({theme}) => (theme.dark ? LightBlack : Slate30)};
+  border-radius: 12px;
   flex-direction: column;
+  margin: 0 ${ScreenGutter} ${ScreenGutter};
+  padding: 16px;
+`;
+
+const ChainSelectionRow = styled.View`
+  flex-direction: row;
+`;
+
+const TokenSelectionRow = styled.View`
+  flex-direction: row;
+  margin-bottom: 24px;
+`;
+
+const ChainDescription = styled(H7)`
+  color: ${({theme}) => (theme.dark ? Slate10 : LightBlack)};
+`;
+
+const CurrencyColumn = styled.View`
   justify-content: center;
+  margin-right: 8px;
+`;
+
+const CurrencyTitleColumn = styled(CurrencyColumn)`
+  flex: 1 1 auto;
+`;
+
+const CurrencyTitle = styled(H7).attrs(() => ({
+  medium: true,
+}))`
+  margin: 0;
+  padding: 0;
+`;
+
+const CurrencySubTitle = styled(BaseText)`
+  color: ${({theme}) => (theme.dark ? LuckySevens : SlateDark)};
+  font-size: 12px;
+`;
+
+const TokensHeading = styled(H7).attrs(() => ({
+  medium: true,
+}))`
+  color: ${({theme}) => (theme.dark ? Slate10 : LuckySevens)};
+  font-weight: 500;
+  margin: 16px 0;
+`;
+
+const TokensFooter = styled.View`
+  align-items: center;
+`;
+
+const ViewAllLink = styled(H6)`
+  color: ${({theme}) => theme.colors.link};
+  text-align: center;
 `;
 
 const CurrencySelectionRow: React.VFC<CurrencySelectionRowProps> = ({
@@ -56,7 +112,7 @@ const CurrencySelectionRow: React.VFC<CurrencySelectionRowProps> = ({
   onViewAllTokensPressed,
 }) => {
   const {t} = useTranslation();
-  const {id, currencyAbbreviation, currencyName, img, selected, disabled} =
+  const {currencyAbbreviation, currencyName, img, selected, disabled} =
     currency;
   const onPress = (item: CurrencySelectionItem): void => {
     haptic(IS_ANDROID ? 'keyboardPress' : 'impactLight');
@@ -69,54 +125,80 @@ const CurrencySelectionRow: React.VFC<CurrencySelectionRowProps> = ({
   };
 
   return (
-    <RowContainer
-      activeOpacity={ActiveOpacity}
-      onPress={() => onPress(currency)}>
-      <CurrencyImageContainer>
-        <CurrencyImage img={img} />
-      </CurrencyImageContainer>
+    <CurrencySelectionRowContainer>
+      <ChainSelectionRow>
+        <CurrencyColumn>
+          <CurrencyImage img={img} />
+        </CurrencyColumn>
 
-      <CurrencyColumn>
-        <H5>{currencyName}</H5>
+        <CurrencyTitleColumn style={{flexGrow: 1}}>
+          <CurrencyTitle>{currencyName}</CurrencyTitle>
 
-        <ListItemSubText>{currencyAbbreviation}</ListItemSubText>
+          <CurrencySubTitle>{currencyAbbreviation}</CurrencySubTitle>
+        </CurrencyTitleColumn>
 
-        {description ? <ListItemSubText>{description}</ListItemSubText> : null}
+        {!hideCheckbox && (
+          <CurrencyColumn>
+            <Checkbox
+              checked={!!selected}
+              disabled={disabled}
+              onPress={() => onPress(currency)}
+            />
+          </CurrencyColumn>
+        )}
+      </ChainSelectionRow>
 
-        {tokens?.length ? (
-          <>
-            {tokens.slice(0, 3).map(token => (
-              <View key={id + ':' + token.id} style={{flexDirection: 'row'}}>
-                <ListItemSubText style={{flexShrink: 1, flexGrow: 1}}>
+      {description ? (
+        <ChainSelectionRow style={{marginTop: 16}}>
+          <ChainDescription>{description}</ChainDescription>
+        </ChainSelectionRow>
+      ) : null}
+
+      {tokens?.length ? (
+        <>
+          <TokensHeading>
+            {t('PopularArgTokens', {currency: t(currencyName)})}
+          </TokensHeading>
+
+          {tokens.slice(0, 3).map(token => (
+            <TokenSelectionRow key={token.id}>
+              <CurrencyColumn style={{marginRight: 16}}>
+                <ArrowDownRightIcon />
+              </CurrencyColumn>
+
+              <CurrencyColumn>
+                <CurrencyImage img={token.img} />
+              </CurrencyColumn>
+
+              <CurrencyTitleColumn style={{flexGrow: 1}}>
+                <CurrencyTitle>{token.currencyName}</CurrencyTitle>
+
+                <CurrencySubTitle style={{flexShrink: 1, flexGrow: 1}}>
                   {token.currencyAbbreviation}
-                </ListItemSubText>
+                </CurrencySubTitle>
+              </CurrencyTitleColumn>
+
+              <CurrencyColumn>
                 <Checkbox
                   checked={!!token.selected}
                   disabled={false}
                   onPress={() => onPress(token)}
                 />
-              </View>
-            ))}
-            <ListItemSubText
+              </CurrencyColumn>
+            </TokenSelectionRow>
+          ))}
+
+          <TokensFooter>
+            <ViewAllLink
               onPress={() => {
                 onViewAllTokensPressed?.(currency, tokens);
               }}>
-              {t('SeeAllArgTokens', {currency: t(currencyName)})}
-            </ListItemSubText>
-          </>
-        ) : null}
-      </CurrencyColumn>
-
-      {!hideCheckbox && (
-        <CheckBoxContainer>
-          <Checkbox
-            checked={!!selected}
-            disabled={disabled}
-            onPress={() => onPress(currency)}
-          />
-        </CheckBoxContainer>
-      )}
-    </RowContainer>
+              {t('ViewAllArgTokens', {currency: t(currencyName)})}
+            </ViewAllLink>
+          </TokensFooter>
+        </>
+      ) : null}
+    </CurrencySelectionRowContainer>
   );
 };
 
