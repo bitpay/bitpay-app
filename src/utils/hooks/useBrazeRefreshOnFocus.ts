d1191f400bac35b1e07249c107316a4b25f4f33d
Chris Nguyen <cnguyen@bitpay.com>
2022-08-10 11:32:33 -0700

    Fix linting error

--- src/utils/hooks/useBrazeRefreshOnFocus.ts
+++ src/utils/hooks/useBrazeRefreshOnFocus.ts
@@ -1,6 +1,6 @@
 import {useFocusEffect} from '@react-navigation/native';
 import throttle from 'lodash.throttle';
-import {useCallback} from 'react';
+import {useMemo} from 'react';
 import {AppEffects} from '../../store/app';
 import useAppDispatch from './useAppDispatch';
 
@@ -8,15 +8,16 @@ export const useBrazeRefreshOnFocus = () => {
   const dispatch = useAppDispatch();
 
   useFocusEffect(
-    useCallback(
-      throttle(
-        () => {
-          dispatch(AppEffects.requestBrazeContentRefresh());
-        },
-        10 * 1000,
-        {leading: true, trailing: false},
-      ),
-      [],
+    useMemo(
+      () =>
+        throttle(
+          () => {
+            dispatch(AppEffects.requestBrazeContentRefresh());
+          },
+          10 * 1000,
+          {leading: true, trailing: false},
+        ),
+      [dispatch],
     ),
   );
 };
