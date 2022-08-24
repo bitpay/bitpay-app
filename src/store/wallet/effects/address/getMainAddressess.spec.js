aa0fd34b19bea7d170b817d09cf7026d0ae8f1d8
Travis Hoglund <thoglund@bitpay.com>
2022-08-24 16:59:14 -0500

    Finished wallet:address tests

---
+++ src/store/wallet/effects/address/getMainAddressess.spec.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {GetMainAddresses} from './address';
+
+/**
+ * getMainAddresses Tests
+ */
+describe('getMainAddresses', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('will respond with addresses if no errors are in callback', async () => {
+    const result = await GetMainAddresses(
+      {
+        getMainAddresses: jest.fn().mockImplementation((opts, callback) => {
+          callback(false, 'ADDRESSES');
+        }),
+      },
+      {
+        doNotVerify: true,
+      },
+    );
+    expect(result).toEqual('ADDRESSES');
+  });
+
+  it('will reject promise if callback has error', async () => {
+    await expect(
+      GetMainAddresses(
+        {
+          getMainAddresses: jest.fn().mockImplementation((opts, callback) => {
+            callback('ERROR');
+          }),
+        },
+        {
+          doNotVerify: true,
+        },
+      ),
+    ).rejects.toEqual('ERROR');
+  });
+});
