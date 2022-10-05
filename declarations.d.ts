4f6719c709f5684487c87ab8a19aefd6e31dea50
Chris Nguyen <cnguyen@bitpay.com>
2022-10-05 10:50:32 -0700

    Add additiional ENV variables for Braze

--- declarations.d.ts
+++ declarations.d.ts
@@ -6,6 +6,8 @@ declare module '*.svg' {
 
 declare module '@env' {
   export const COINBASE_CLIENT_ID: string;
+  export const BRAZE_EXPORT_API_KEY: string;
+  export const BRAZE_REST_API_ENDPOINT: string;
   export const COINBASE_CLIENT_SECRET: string;
   export const DOSH_APP_ID: string;
   export const DOSH_WHITELIST: string;
