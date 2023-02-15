defa36cdb89d9934211f3d017a0f64ae843f06f3
Gabriel <gabrielbazan89@gmail.com>
2023-02-15 13:28:51 -0300

    [FIX] encrypt password navigation

--- src/navigation/wallet/screens/KeyOverview.tsx
+++ src/navigation/wallet/screens/KeyOverview.tsx
@@ -5,7 +5,13 @@ import React, {
   useMemo,
   useState,
 } from 'react';
-import {CommonActions, useTheme} from '@react-navigation/native';
+import {
+  CommonActions,
+  RouteProp,
+  useNavigation,
+  useRoute,
+  useTheme,
+} from '@react-navigation/native';
 import {FlatList, LogBox, RefreshControl, TouchableOpacity} from 'react-native';
 import styled from 'styled-components/native';
 import haptic from '../../../components/haptic-feedback/haptic';
@@ -72,11 +78,6 @@ import CoinbaseDropdownOption from '../components/CoinbaseDropdownOption';
 LogBox.ignoreLogs([
   'Non-serializable values were found in the navigation state',
 ]);
-
-type KeyOverviewScreenProps = StackScreenProps<
-  WalletStackParamList,
-  'KeyOverview'
->;
 
 const Row = styled.View`
   flex-direction: row;
@@ -342,14 +343,17 @@ export const buildNestedWalletList = (
   return walletList;
 };
 
-const KeyOverview: React.FC<KeyOverviewScreenProps> = ({navigation, route}) => {
+const KeyOverview = () => {
   const {t} = useTranslation();
+  const {
+    params: {id, context},
+  } = useRoute<RouteProp<WalletStackParamList, 'KeyOverview'>>();
+  const navigation = useNavigation();
   const dispatch = useAppDispatch();
   const logger = useLogger();
   const theme = useTheme();
   const [showKeyOptions, setShowKeyOptions] = useState(false);
   const [refreshing, setRefreshing] = useState(false);
-  const {id, context} = route.params;
   const {keys} = useAppSelector(({WALLET}) => WALLET);
   const {rates} = useAppSelector(({RATE}) => RATE);
   const defaultAltCurrency = useAppSelector(({APP}) => APP.defaultAltCurrency);
@@ -410,8 +414,11 @@ const KeyOverview: React.FC<KeyOverviewScreenProps> = ({navigation, route}) => {
               {key?.methods?.isPrivKeyEncrypted() ? (
                 <CogIconContainer
                   onPress={() =>
-                    navigation.navigate('KeySettings', {
-                      key,
+                    navigation.navigate('Wallet', {
+                      screen: 'KeySettings',
+                      params: {
+                        key,
+                      },
                     })
                   }
                   activeOpacity={ActiveOpacity}>
@@ -443,9 +450,12 @@ const KeyOverview: React.FC<KeyOverviewScreenProps> = ({navigation, route}) => {
               err instanceof Error ? err.message : JSON.stringify(err);
             logger.error(`error [getStatus]: ${errStr}`);
           } else {
-            navigation.navigate('Copayers', {
-              wallet: key?.wallets[0],
-              status: status?.wallet,
+            navigation.navigate('Wallet', {
+              screen: 'Copayers',
+              params: {
+                wallet: key?.wallets[0],
+                status: status?.wallet,
+              },
             });
           }
         },
@@ -489,9 +499,12 @@ const KeyOverview: React.FC<KeyOverviewScreenProps> = ({navigation, route}) => {
         ),
         onPress: () => {
           haptic('impactLight');
-          navigation.navigate('KeySettings', {
-            key,
-            context: 'createEncryptPassword',
+
+          navigation.navigate('Wallet', {
+            screen: 'CreateEncryptPassword',
+            params: {
+              key,
+            },
           });
         },
       });
@@ -503,8 +516,11 @@ const KeyOverview: React.FC<KeyOverviewScreenProps> = ({navigation, route}) => {
       description: t('View all the ways to manage and configure your key.'),
       onPress: () => {
         haptic('impactLight');
-        navigation.navigate('KeySettings', {
-          key,
+        navigation.navigate('Wallet', {
+          screen: 'KeySettings',
+          params: {
+            key,
+          },
         });
       },
     });
@@ -512,7 +528,10 @@ const KeyOverview: React.FC<KeyOverviewScreenProps> = ({navigation, route}) => {
 
   const onPressTxpBadge = useMemo(
     () => () => {
-      navigation.navigate('TransactionProposalNotifications', {keyId: key.id});
+      navigation.navigate('Wallet', {
+        screen: 'TransactionProposalNotifications',
+        params: {keyId: key.id},
+      });
     },
     [],
   );
@@ -553,24 +572,33 @@ const KeyOverview: React.FC<KeyOverviewScreenProps> = ({navigation, route}) => {
                   } else {
                     if (status?.wallet?.status === 'complete') {
                       fullWalletObj.openWallet({}, () => {
-                        navigation.navigate('WalletDetails', {
-                          walletId: item.id,
-                          key,
+                        navigation.navigate('Wallet', {
+                          screen: 'WalletDetails',
+                          params: {
+                            walletId: item.id,
+                            key,
+                          },
                         });
                       });
                       return;
                     }
-                    navigation.navigate('Copayers', {
-                      wallet: fullWalletObj,
-                      status: status?.wallet,
+                    navigation.navigate('Wallet', {
+                      screen: 'Copayers',
+                      params: {
+                        wallet: fullWalletObj,
+                        status: status?.wallet,
+                      },
                     });
                   }
                 },
               );
             } else {
-              navigation.navigate('WalletDetails', {
-                walletId: item.id,
-                key,
+              navigation.navigate('Wallet', {
+                screen: 'WalletDetails',
+                params: {
+                  key,
+                  walletId: item.id,
+                },
               });
             }
           }}
@@ -624,8 +652,11 @@ const KeyOverview: React.FC<KeyOverviewScreenProps> = ({navigation, route}) => {
               activeOpacity={ActiveOpacity}
               onPress={() => {
                 haptic('impactLight');
-                navigation.navigate('AddingOptions', {
-                  key,
+                navigation.navigate('Wallet', {
+                  screen: 'AddingOptions',
+                  params: {
+                    key,
+                  },
                 });
               }}>
               <Icons.Add />
@@ -666,7 +697,7 @@ const KeyOverview: React.FC<KeyOverviewScreenProps> = ({navigation, route}) => {
                     setShowKeyDropdown(false);
                     navigation.setParams({
                       id: keyId,
-                    });
+                    } as any);
                   }}
                   defaultAltCurrencyIsoCode={defaultAltCurrency.isoCode}
                   hideKeyBalance={_key.hideKeyBalance}
