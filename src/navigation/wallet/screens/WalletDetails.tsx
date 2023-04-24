780b9a82cd39e6d13b9ae817f7ab0070deebeaa0
Gabriel <gabrielbazan89@gmail.com>
2023-04-24 13:13:49 -0300

    [REF] RBF transaction row

--- src/navigation/wallet/screens/WalletDetails.tsx
+++ src/navigation/wallet/screens/WalletDetails.tsx
@@ -889,6 +889,7 @@ const WalletDetails: React.FC<WalletDetailsScreenProps> = ({route}) => {
         description={item.uiDescription}
         time={item.uiTime}
         value={item.uiValue}
+        RBFInfo={{isRBF: item.isRBF, isReceived: IsReceived(item.action)}}
         onPressTransaction={() => onPressTransaction(item)}
       />
     );
