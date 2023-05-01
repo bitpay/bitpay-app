bff0835f0dd049e2d679e1e335fad20c0bc4c96f
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2023-05-01 10:38:35 -0300

    Ref: optimize checking wallets

--- src/navigation/wallet/screens/send/confirm/Shared.tsx
+++ src/navigation/wallet/screens/send/confirm/Shared.tsx
@@ -550,21 +550,19 @@ export const WalletSelector = ({
   useMemo(() => {
     let hasWallets: boolean = false;
     let hasCoinbase: boolean = false;
-    for (const keyWallet of walletsAndAccounts.keyWallets) {
-      if (keyWallet.wallets.length > 0) {
-        hasWallets = true;
-      }
+
+    const {keyWallets, coinbaseWallets} = walletsAndAccounts;
+    if (keyWallets.length > 0 && keyWallets[0].wallets.length > 0) {
+      hasWallets = true;
     }
-    for (const coinbaseWallet of walletsAndAccounts.coinbaseWallets) {
-      if (coinbaseWallet.wallets.length > 0) {
-        hasCoinbase = true;
-      }
+    if (coinbaseWallets.length > 0 && coinbaseWallets[0].wallets.length > 0) {
+      hasCoinbase = true;
     }
 
     if (!hasWallets && !hasCoinbase) {
       setShowNoWallets(true);
     }
-  }, [dispatch, navigation, walletsAndAccounts]);
+  }, [walletsAndAccounts]);
 
   useEffect(() => {
     isVisible && !showNoWallets
