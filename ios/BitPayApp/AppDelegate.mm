d8d9c76ac64124a84a20ee6462f96a0d244bb328
Gabriel <gabrielbazan89@gmail.com>
2023-05-16 09:47:37 -0300

    [FIX] remove dup concurrentRootEnabled

--- ios/BitPayApp/AppDelegate.mm
+++ ios/BitPayApp/AppDelegate.mm
@@ -84,16 +84,6 @@
 #endif
 }
 
-/// This method controls whether the `concurrentRoot`feature of React18 is turned on or off.
-///
-/// @see: https://reactjs.org/blog/2022/03/29/react-v18.html
-/// @note: This requires to be rendering on Fabric (i.e. on the New Architecture).
-/// @return: `true` if the `concurrentRoot` feature is enabled. Otherwise, it returns `false`.
-- (BOOL)concurrentRootEnabled
-{
-  return true;
-}
-
 // Deep linking
 // Open URI-scheme for iOS 9 and above
 - (BOOL)application:(UIApplication *)application
