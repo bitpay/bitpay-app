b3a7aca2504074a5a63ca6abb4735ef0325e3777
Gustavo Cortez <cmgustavo83@gmail.com>
2023-05-29 11:16:56 -0300

    Feat: global error handler to capture uncaught exceptions. New Events

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ import 'react-native-get-random-values'; // must import before @ethersproject/sh
 import '@ethersproject/shims';
 import './shim';
 import '@walletconnect/react-native-compat';
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Alert} from 'react-native';
 import Root from './src/Root';
 import React from 'react';
 import './i18n';
+import {
+  setJSExceptionHandler,
+  setNativeExceptionHandler,
+} from 'react-native-exception-handler';
 import {name as appName} from './app.json';
 import getStore from './src/store';
 import {Provider} from 'react-redux';
@@ -13,10 +17,50 @@ import {PersistGate} from 'redux-persist/integration/react';
 import 'react-native-url-polyfill/auto'; // https://github.com/facebook/react-native/issues/23922#issuecomment-648096619
 import {enableFreeze} from 'react-native-screens';
 import {AppInitialization} from './src/AppInitialization';
-
+import {Analytics} from './src/store/analytics/analytics.effects';
+import {APP_VERSION} from './src/constants/config';
+import {GIT_COMMIT_HASH} from '@env';
+import {LogActions} from './src/store/log';
 enableFreeze(true);
 
 const {store, persistor} = getStore();
+
+const errorHandler = (e, isFatal) => {
+  if (isFatal) {
+    store.dispatch(
+      Analytics.track('BitPay App - Crashed App', {
+        build: GIT_COMMIT_HASH,
+        version: APP_VERSION,
+      }),
+    );
+    const errStr = e instanceof Error ? e.message : JSON.stringify(e);
+    store.dispatch(LogActions.persistLog(LogActions.error(errStr)));
+    Alert.alert(
+      'Unexpected error occurred',
+      `
+        Error: ${errStr}
+        `,
+      [
+        {
+          text: 'Close',
+        },
+      ],
+    );
+  }
+};
+
+const exceptionHandler = () => {
+  store.dispatch(
+    Analytics.track('BitPay App - Crashed App', {
+      build: GIT_COMMIT_HASH,
+      version: APP_VERSION,
+    }),
+  );
+};
+
+// Handle uncaught exceptions
+setJSExceptionHandler(errorHandler, true);
+setNativeExceptionHandler(exceptionHandler);
 
 const ReduxProvider = () => {
   return (
