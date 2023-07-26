0e2aa28bf1c99f012368d86e7d366df1a4bf93f0
Chris Nguyen <cnguyen@bitpay.com>
2023-07-26 11:47:49 -0700

    Ensure non-null value passed to NativeEventEmitter

--- src/lib/google-push-provisioning/GooglePushProvisioning.ts
+++ src/lib/google-push-provisioning/GooglePushProvisioning.ts
@@ -1,12 +1,17 @@
 import ReactNative from 'react-native';
 import {NativeEventEmitter} from 'react-native';
 
+const NO_OP = () => null;
+
 interface GooglePushProvisioningModule {
   startPushProvision: any;
 }
 
-const module = ReactNative.NativeModules?.GooglePushProvisioning || null;
-const GooglePushProvisioning = (module || {}) as GooglePushProvisioningModule;
+const module = ReactNative.NativeModules?.GooglePushProvisioning || {
+  addListener: NO_OP,
+  removeListeners: NO_OP,
+};
+const GooglePushProvisioning = module as GooglePushProvisioningModule;
 const eventEmitter = new NativeEventEmitter(module);
 
 const startPushProvision = (
