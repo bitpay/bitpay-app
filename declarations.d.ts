b013dfdb364a2931adfc1ccaff326c78686c2a69
Chris Nguyen <cnguyen@bitpay.com>
2023-08-08 10:21:56 -0700

    Add MIXPANEL_PROJECT_TOKEN to env template

--- declarations.d.ts
+++ declarations.d.ts
@@ -5,16 +5,17 @@ declare module '*.svg' {
 }
 
 declare module '@env' {
-  export const COINBASE_CLIENT_ID: string;
   export const BRAZE_EXPORT_API_KEY: string;
   export const BRAZE_REST_API_ENDPOINT: string;
+  export const COINBASE_CLIENT_ID: string;
   export const COINBASE_CLIENT_SECRET: string;
   export const DOSH_APP_ID: string;
   export const DOSH_WHITELIST: string;
+  export const GIT_COMMIT_HASH: string;
   export const SEGMENT_API_KEY_ANDROID: string;
   export const SEGMENT_API_KEY_IOS: string;
-  export const GIT_COMMIT_HASH: string;
-  export const ZENLEDGER_CLIENT_ID: string;
-  export const WALLET_CONNECT_V2_PROJECT_ID: string;
+  export const MIXPANEL_PROJECT_TOKEN: string;
   export const MORALIS_API_KEY: string;
+  export const WALLET_CONNECT_V2_PROJECT_ID: string;
+  export const ZENLEDGER_CLIENT_ID: string;
 }
