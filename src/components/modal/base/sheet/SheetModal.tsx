6aee54869337dbe1bd7849836d51fe5f16a79aa4
Gustavo Cortez <cmgustavo83@gmail.com>
2023-10-04 10:01:42 -0300

    Fix: dismiss SheetModal on background mode

--- src/components/modal/base/sheet/SheetModal.tsx
+++ src/components/modal/base/sheet/SheetModal.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {BlurContainer} from '../../../blur/Blur';
 import {SheetParams} from '../../../styled/Containers';
 import BaseModal from '../BaseModal';
+import {AppState, AppStateStatus} from 'react-native';
 
 interface Props extends SheetParams {
   isVisible: boolean;
   onBackdropPress: (props?: any) => void;
+  children?: any;
 }
 
 const SheetModal: React.FC<Props> = ({
@@ -14,10 +16,26 @@ const SheetModal: React.FC<Props> = ({
   onBackdropPress,
   placement,
 }) => {
+  const [isModalVisible, setModalVisible] = useState(isVisible);
+  useEffect(() => {
+    function onAppStateChange(status: AppStateStatus) {
+      if (status === 'background') {
+        setModalVisible(false);
+      }
+    }
+    setModalVisible(isVisible);
+
+    const subscriptionAppStateChange = AppState.addEventListener(
+      'change',
+      onAppStateChange,
+    );
+
+    return () => subscriptionAppStateChange.remove();
+  }, [isVisible]);
   return (
     <BaseModal
       id={'sheetModal'}
-      isVisible={isVisible}
+      isVisible={isModalVisible}
       backdropOpacity={0.4}
       backdropTransitionOutTiming={0}
       hideModalContentWhileAnimating={true}
