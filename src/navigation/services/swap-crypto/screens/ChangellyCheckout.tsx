8d0006031fc08f81fbeb2a23d658e48eb36aac13
Gabriel Masclef <gabrielmasclef@hotmail.com>
2023-11-15 16:37:53 -0300

    Ref: disable slide to swap if checkbox is not selected

--- src/navigation/services/swap-crypto/screens/ChangellyCheckout.tsx
+++ src/navigation/services/swap-crypto/screens/ChangellyCheckout.tsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from 'react';
-import {ScrollView} from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
+import {ScrollView, TouchableOpacity} from 'react-native';
 import {
   useTheme,
   RouteProp,
@@ -127,9 +127,11 @@ const ChangellyCheckout: React.FC = () => {
   const dispatch = useAppDispatch();
   const theme = useTheme();
   const BWC = BwcProvider.getInstance();
+  const scrollViewRef = useRef<ScrollView>(null);
 
   const [isLoading, setIsLoading] = useState(true);
   const [termsAccepted, setTermsAccepted] = useState(false);
+  const [showCheckTermsMsg, setShowCheckTermsMsg] = useState(false);
   const [remainingTimeStr, setRemainingTimeStr] = useState<string>('');
   const [amountExpectedFrom, setAmountExpectedFrom] =
     useState<number>(amountFrom);
@@ -712,7 +714,7 @@ const ChangellyCheckout: React.FC = () => {
 
   return (
     <SwapCheckoutContainer>
-      <ScrollView>
+      <ScrollView ref={scrollViewRef}>
         <RowDataContainer>
           <H5>{t('SUMMARY')}</H5>
         </RowDataContainer>
@@ -847,11 +849,19 @@ const ChangellyCheckout: React.FC = () => {
                 </FiatAmountContainer>
               </>
             )}
+            {!termsAccepted && showCheckTermsMsg ? (
+              <RowDataContainer>
+                <RowLabel style={{color: Caution}}>
+                  {t('Tap the checkbox to accept and continue.')}
+                </RowLabel>
+              </RowDataContainer>
+            ) : null}
             <CheckBoxContainer>
               <Checkbox
                 radio={false}
                 onPress={() => {
                   setTermsAccepted(!termsAccepted);
+                  setShowCheckTermsMsg(!!termsAccepted);
                 }}
                 checked={termsAccepted}
               />
@@ -880,18 +890,27 @@ const ChangellyCheckout: React.FC = () => {
         )}
       </ScrollView>
 
-      {termsAccepted && !paymentExpired && !!exchangeTxId && (
-        <SwipeButton
-          title={'Slide to send'}
-          onSwipeComplete={async () => {
-            try {
-              logger.debug('Swipe completed. Making payment...');
-              makePayment();
-            } catch (err) {}
-          }}
-          forceReset={resetSwipeButton}
-        />
-      )}
+      {!paymentExpired && !!exchangeTxId ? (
+        <TouchableOpacity
+          onPress={() => {
+            if (!termsAccepted) {
+              scrollViewRef?.current?.scrollToEnd({animated: true});
+            }
+            setShowCheckTermsMsg(!termsAccepted);
+          }}>
+          <SwipeButton
+            title={'Slide to send'}
+            disabled={!termsAccepted}
+            onSwipeComplete={async () => {
+              try {
+                logger.debug('Swipe completed. Making payment...');
+                makePayment();
+              } catch (err) {}
+            }}
+            forceReset={resetSwipeButton}
+          />
+        </TouchableOpacity>
+      ) : null}
 
       <ChangellyPoliciesModal
         isVisible={changellyPoliciesModalVisible}
