8d07d03a28393e8748ed93238174cdb9fb7a7258
Gustavo Cortez <cmgustavo83@gmail.com>
2023-11-16 12:19:19 -0300

    To lowercase token address

--- src/constants/SupportedCurrencyOptions.tsx
+++ src/constants/SupportedCurrencyOptions.tsx
@@ -280,7 +280,7 @@ export const SupportedTokenOptions: Array<SupportedCurrencyOption> = [
     imgSrc: require('../../assets/img/currencies/png/USDC.png'),
     badgeSrc: require('../../assets/img/currencies/png/MATIC.png'),
     badgeUri: CurrencyListIcons.matic,
-    tokenAddress: '0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359',
+    tokenAddress: '0x3c499c542cef5e3811e1192ce70d8cc03d5c3359',
   },
   {
     id: Math.random().toString(),
