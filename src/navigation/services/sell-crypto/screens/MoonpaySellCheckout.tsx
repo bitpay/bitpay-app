a827c5a3ca180293d8eff77caadf2a804deab523
Gabriel Masclef <gabrielmasclef@hotmail.com>
2024-03-15 16:40:33 -0300

    Fix: Ensure sell failed analytics is called once

--- src/navigation/services/sell-crypto/screens/MoonpaySellCheckout.tsx
+++ src/navigation/services/sell-crypto/screens/MoonpaySellCheckout.tsx
@@ -149,6 +149,8 @@ const MoonpaySellCheckout: React.FC = () => {
   const [showCheckTermsMsg, setShowCheckTermsMsg] = useState(false);
   const [showNewQuoteTermsMsg, setShowNewQuoteTermsMsg] = useState(false);
   const [remainingTimeStr, setRemainingTimeStr] = useState<string>('');
+  const [expiredAnalyticSent, setExpiredAnalyticSent] =
+    useState<boolean>(false);
   const [amountExpected, setAmountExpected] = useState<number>(amount);
   const [fee, setFee] = useState<number>();
   const [ctxp, setCtxp] = useState<Partial<TransactionProposal>>();
@@ -189,21 +191,10 @@ const MoonpaySellCheckout: React.FC = () => {
 
     if (now > expirationTime) {
       setPaymentExpired(true);
-      setRemainingTimeStr('Expired');
+      setRemainingTimeStr('expired');
       if (countDown) {
         clearInterval(countDown);
       }
-      dispatch(
-        Analytics.track('Failed Crypto Sell', {
-          exchange: 'moonpay',
-          context: 'MoonpaySellCheckout',
-          reasonForFailure: 'Time to make the payment expired',
-          amountFrom: amountExpected || '',
-          fromCoin: wallet.currencyAbbreviation.toLowerCase() || '',
-          fiatAmount: sellOrder?.fiat_receiving_amount || '',
-          fiatCurrency: sellOrder?.fiat_currency?.toLowerCase() || '',
-        }),
-      );
       return;
     }
 
@@ -689,6 +680,23 @@ const MoonpaySellCheckout: React.FC = () => {
   }, []);
 
   useEffect(() => {
+    if (remainingTimeStr === 'expired' && !expiredAnalyticSent) {
+      dispatch(
+        Analytics.track('Failed Crypto Sell', {
+          exchange: 'moonpay',
+          context: 'MoonpaySellCheckout',
+          reasonForFailure: 'Time to make the payment expired',
+          amountFrom: amountExpected || '',
+          fromCoin: wallet.currencyAbbreviation.toLowerCase() || '',
+          fiatAmount: sellOrder?.fiat_receiving_amount || '',
+          fiatCurrency: sellOrder?.fiat_currency?.toLowerCase() || '',
+        }),
+      );
+      setExpiredAnalyticSent(true);
+    }
+  }, [remainingTimeStr, expiredAnalyticSent]);
+
+  useEffect(() => {
     if (!resetSwipeButton) {
       return;
     }
@@ -847,7 +855,9 @@ const MoonpaySellCheckout: React.FC = () => {
                       ? White
                       : Black,
                   }}>
-                  {remainingTimeStr}
+                  {remainingTimeStr === 'expired'
+                    ? t('Expired')
+                    : remainingTimeStr}
                 </RowData>
               )}
             </RowDataContainer>
