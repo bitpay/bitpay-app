34b170077cac3e2bea20d1ee4b16ba1805435715
Gabriel Masclef <gabrielmasclef@hotmail.com>
2024-06-05 10:15:00 -0300

    Swap: new chains support

--- src/navigation/services/swap-crypto/utils/changelly-utils.ts
+++ src/navigation/services/swap-crypto/utils/changelly-utils.ts
@@ -1,67 +1,92 @@
 import {t} from 'i18next';
 import {Wallet} from '../../../../store/wallet/wallet.models';
 
+export const changellySupportedUtxoChains = [
+  // UTXO
+  'bitcoin',
+  'bitcoin_cash',
+  'doge',
+  'litecoin',
+];
+
+export const changellySupportedEvmChains = [
+  // EVM
+  'ethereum',
+  'arbitrum',
+  'base', // BASE in Changelly
+  'optimism',
+  'polygon',
+];
+
+export const changellySupportedOtherChains = [
+  // Ohters
+  'ripple',
+];
+
 export const changellySupportedCoins = [
   'btc',
   'bch',
   'eth',
+  'eth_arb', // etharb in Changelly
+  'eth_op', // ethop in Changelly
   'doge',
-  'matic', // maticpolygon in Changelly
   'ltc',
+  'matic', // maticpolygon in Changelly
   'xrp',
 ];
 
 export const changellySupportedEthErc20Tokens = [
-  'usdp',
-  'usdc',
-  'gusd',
-  'dai',
-  'wbtc',
-  'shib',
-  'ape',
   '1inch',
   'aave',
+  'aevo',
+  'agi',
   'agix',
+  'aioz',
   'alcx',
   'alice',
   'alpha',
+  'alt',
   'amp',
   'ankr',
-  'ant',
+  'ape',
   'api3',
   'arkm',
-  'arpa',
+  'ast',
   'audio',
   'axs',
   'bal',
   'band',
   'bat',
+  'bnt',
   'blur',
   'blz',
-  'bnt',
+  'bone',
   'celr',
-  'chr',
   'chz',
-  'combo',
   'comp',
-  'croold',
+  'combo',
+  'coval',
   'crv',
   'cvc',
   'cvx',
-  'dnt',
+  'dai',
+  'dao',
+  'dia',
   'dodo',
   'dydx',
   'elon',
-  'enj',
   'ens',
+  'ena',
+  'eth',
+  'ethfi',
   'eurs',
   'eurt',
-  'farm',
   'forth',
   'ftm',
-  'ftt',
+  'fun',
   'fxs',
   'gala',
+  'gno',
   'glm',
   'grt',
   'hot',
@@ -72,64 +97,73 @@ export const changellySupportedEthErc20Tokens = [
   'jasmy',
   'knc',
   'ldo',
-  'leo',
-  'lina',
   'link',
-  'loom',
+  'lina',
   'lpt',
+  'looks',
   'lqty',
   'lrc',
   'mana',
-  'mask',
   'matic', // erc20
-  'mir',
+  'metis',
   'mkr',
+  'mtl',
   'nexo',
   'nmr',
+  'noia',
   'ocean',
   'ogn',
   'omg',
+  'omni',
+  'ondo',
   'paxg',
   'pepe',
+  'pendle',
   'perp',
-  'poly',
+  'pixel',
   'pond',
   'powr',
   'pyusd',
   'qnt',
   'rad',
-  'rare',
+  'radar',
+  'rari',
   'reef',
   'ren',
-  'rep',
   'req',
+  'rez',
   'rlc',
   'rndr',
+  'rpl',
   'rsr',
   'sand',
+  'shib',
+  'sis',
   'skl',
   'slp',
+  'snt',
   'snx',
+  'steth',
   'stg',
   'stmx',
   'storj',
-  'stpt',
   'sushi',
   'syn',
+  'tower',
   'trb',
   'tru',
   'tusd',
-  'tvk',
   'uma',
   'uni',
-  'uos',
+  'usdc',
+  'usdp',
   'usdt', // usdt20 in Changelly
-  'vgx',
-  'vib',
-  'vidt',
+  'vra',
+  'wbtc',
+  'wnxm',
   'woo',
+  'xaut',
   'yfi',
-  'yfii',
   'ygg',
   'zrx',
 ];
@@ -141,6 +175,55 @@ export const changellySupportedMaticErc20Tokens = [
   'usdc', // usdcmatic in Changelly
 ];
 
+export const changellySupportedArbitrumTokens = [
+  'aeur',
+  'aidoge',
+  'arb',
+  'dpx',
+  'magic',
+  'rdnt',
+  'spa',
+  'usdc', // usdcarb in Changelly
+  'vela',
+  'vrtx',
+  'xai',
+];
+
+export const changellySupportedBaseTokens = [
+  'aero',
+  'base',
+  'brett',
+  'degen',
+  'usdc', // usdcbase in Changelly
+];
+
+export const changellySupportedOptimismTokens = [
+  'op',
+  'usdc', // usdcop in Changelly
+  'wld',
+];
+
+const supportedTokensByChain: {[key: string]: string[]} = {
+  eth: changellySupportedEthErc20Tokens,
+  matic: changellySupportedMaticErc20Tokens,
+  arb: changellySupportedArbitrumTokens,
+  base: changellySupportedBaseTokens,
+  op: changellySupportedOptimismTokens,
+};
+
+const blockchainMapping: {[key: string]: string} = {
+  bitcoin: 'btc',
+  bitcoin_cash: 'bch',
+  ethereum: 'eth',
+  doge: 'doge',
+  litecoin: 'ltc',
+  polygon: 'matic',
+  arbitrum: 'arb',
+  base: 'base',
+  optimism: 'op',
+  ripple: 'xrp',
+};
+
 export const generateMessageId = (walletId?: string) => {
   const now = Date.now();
   if (walletId) {
@@ -149,6 +232,18 @@ export const generateMessageId = (walletId?: string) => {
   const randomInt = Math.floor(1e8 * Math.random());
   return `${randomInt}-${now}`;
 };
+
+export type ChangellyCurrencyBlockchain =
+  | 'bitcoin'
+  | 'bitcoin_cash'
+  | 'ethereum'
+  | 'doge'
+  | 'litecoin'
+  | 'polygon'
+  | 'arbitrum'
+  | 'base'
+  | 'optimism'
+  | 'ripple';
 
 export interface ChangellyCurrency {
   name: string; // currencyAbbreviation
@@ -165,7 +260,7 @@ export interface ChangellyCurrency {
   transactionUrl?: string;
   image?: string;
   fixedTime?: number;
-  blockchain?: string;
+  blockchain?: ChangellyCurrencyBlockchain;
   notifications?: {
     payin?: string;
   };
@@ -192,29 +287,90 @@ export interface ChangellyFixTransactionDataType {
   refundAddress: string;
 }
 
+export const getChangellySupportedChains = (
+  chainType?: 'utxo' | 'evm' | 'other',
+) => {
+  switch (chainType) {
+    case 'utxo':
+      return changellySupportedUtxoChains;
+    case 'evm':
+      return changellySupportedEvmChains;
+    case 'other':
+      return changellySupportedOtherChains;
+    default:
+      return [
+        ...changellySupportedUtxoChains,
+        ...changellySupportedEvmChains,
+        ...changellySupportedOtherChains,
+      ];
+  }
+};
+
 export const getChangellyCurrenciesFixedProps = (
   changellyCurrenciesData: ChangellyCurrency[],
 ): ChangellyCurrency[] => {
+  const currencyMapping: {
+    [key: string]: {
+      name: string;
+      fullName?: string;
+      blockchain?: string;
+      contractAddress?: string;
+    };
+  } = {
+    usdt20: {
+      name: 'usdt',
+      fullName: 'Tether USD',
+      blockchain: 'ethereum',
+      contractAddress: '0xdac17f958d2ee523a2206206994597c13d831ec7',
+    },
+    maticpolygon: {name: 'matic', blockchain: 'polygon'},
+    usdcmatic: {
+      name: 'usdc',
+      fullName: 'USD Coin',
+      blockchain: 'polygon',
+      contractAddress: '0x3c499c542cef5e3811e1192ce70d8cc03d5c3359',
+    },
+    etharb: {name: 'eth', fullName: 'Ethereum', blockchain: 'arbitrum'},
+    usdcarb: {
+      name: 'usdc',
+      fullName: 'USD Coin',
+      blockchain: 'arbitrum',
+      contractAddress: '0xaf88d065e77c8cc2239327c5edb3a432268e5831',
+    },
+    usdcbase: {
+      name: 'usdc',
+      fullName: 'USD Coin',
+      blockchain: 'base',
+      contractAddress: '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913',
+    },
+    ethop: {name: 'eth', fullName: 'Ethereum', blockchain: 'optimism'},
+    usdcop: {
+      name: 'usdc',
+      fullName: 'USD Coin',
+      blockchain: 'optimism',
+      contractAddress: '0x0b2c639c533813f4aa9d7837caf62653d097ff85',
+    },
+  };
+
   changellyCurrenciesData.forEach((currency: ChangellyCurrency) => {
+    const key = currency.name.toLowerCase();
+    const mapping = currencyMapping[key];
     if (
-      currency.name.toLowerCase() === 'usdt20' &&
-      currency.protocol?.toLowerCase() === 'erc20' &&
-      currency.contractAddress === '0xdac17f958d2ee523a2206206994597c13d831ec7'
+      mapping &&
+      (!mapping.blockchain ||
+        mapping.blockchain.toLowerCase() ===
+          currency.blockchain?.toLowerCase()) &&
+      (!mapping.contractAddress ||
+        mapping.contractAddress.toLowerCase() ===
+          currency.contractAddress?.toLowerCase())
     ) {
-      currency.name = 'usdt';
-      currency.fullName = 'Tether USD';
-    } else if (
-      currency.name.toLowerCase() === 'maticpolygon' &&
-      currency.protocol?.toLowerCase() === 'matic'
-    ) {
-      currency.name = 'matic';
-    } else if (
-      currency.name.toLowerCase() === 'usdcmatic' &&
-      currency.protocol?.toLowerCase() === 'matic'
-    ) {
-      currency.name = 'usdc';
+      currency.name = mapping.name;
+      if (mapping.fullName) {
+        currency.fullName = mapping.fullName;
+      }
     }
   });
+
   return changellyCurrenciesData;
 };
 
@@ -222,35 +378,62 @@ export const getChangellyFixedCurrencyAbbreviation = (
   currency: string,
   chain: string,
 ): string => {
-  if (currency === 'usdt' && chain === 'eth') {
-    return 'usdt20';
-  } else if (currency === 'matic' && chain === 'matic') {
-    return 'maticpolygon';
-  } else if (currency === 'usdc' && chain === 'matic') {
-    return 'usdcmatic';
-  } else {
-    return currency;
-  }
+  const mapping: {[key: string]: {[key: string]: string}} = {
+    usdt: {eth: 'usdt20'},
+    matic: {matic: 'maticpolygon'},
+    eth: {
+      arbitrum: 'etharb',
+      optimism: 'ethop',
+    },
+    usdc: {
+      matic: 'usdcmatic',
+      arbitrum: 'usdcarb',
+      base: 'usdcbase',
+      optimism: 'usdcop',
+    },
+  };
+
+  return mapping[currency]?.[chain] || currency;
 };
 
-export const isCoinSupportedToSwap = (coin: string, chain: string): boolean => {
-  if (chain === undefined) {
+export const getChainFromChangellyBlockchain = (
+  currencyAbbreviation: string,
+  blockchain?: ChangellyCurrencyBlockchain,
+): string => {
+  if (!blockchain) {
+    return currencyAbbreviation.toLowerCase();
+  }
+
+  const lowerBlockchain = blockchain.toLowerCase();
+  return (
+    blockchainMapping[lowerBlockchain] || currencyAbbreviation.toLowerCase()
+  );
+};
+
+export const isCoinSupportedToSwap = (
+  coin: string,
+  chain?: string,
+): boolean => {
+  const lowerCoin = coin.toLowerCase();
+
+  if (!chain) {
     return [
       ...changellySupportedCoins,
-      ...changellySupportedEthErc20Tokens,
-    ].includes(coin.toLowerCase());
+      ...Object.values(supportedTokensByChain).flat(),
+    ].includes(lowerCoin);
   }
-  if (coin.toLowerCase() === chain.toLowerCase()) {
-    return changellySupportedCoins.includes(coin.toLowerCase());
+
+  const lowerChain = chain.toLowerCase();
+
+  if (lowerCoin === lowerChain) {
+    return changellySupportedCoins.includes(lowerCoin);
   }
-  switch (chain) {
-    case 'eth':
-      return changellySupportedEthErc20Tokens.includes(coin.toLowerCase());
-    case 'matic':
-      return changellySupportedMaticErc20Tokens.includes(coin.toLowerCase());
-    default:
-      return changellySupportedCoins.includes(coin.toLowerCase());
-  }
+
+  const tokens = supportedTokensByChain[lowerChain];
+
+  return tokens
+    ? tokens.includes(lowerCoin)
+    : changellySupportedCoins.includes(lowerCoin);
 };
 
 export const changellyGetFixRateForAmount = async (
