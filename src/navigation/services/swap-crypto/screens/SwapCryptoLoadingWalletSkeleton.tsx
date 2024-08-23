c8125cd4e12a1306cf118776cff13d0c2e99cb3d
Gabriel <gabrielbazan89@gmail.com>
2024-08-23 10:27:58 -0300

    [REF] skeleton loading swap crypto - sell crypto

--- src/navigation/services/swap-crypto/screens/SwapCryptoLoadingWalletSkeleton.tsx
+++ src/navigation/services/swap-crypto/screens/SwapCryptoLoadingWalletSkeleton.tsx
@@ -17,12 +17,27 @@ const SwapCryptoLoadingWalletSkeleton = () => {
           alignItems={'center'}
           justifyContent={'flex-start'}>
           <SkeletonPlaceholder.Item
-            width={20}
-            height={20}
+            width={50}
+            height={50}
             borderRadius={50}
             marginRight={8}
           />
-          <SkeletonPlaceholder.Item width={70} height={14} borderRadius={4} />
+          <SkeletonPlaceholder.Item
+            flexDirection={'column'}
+            alignItems={'center'}
+            justifyContent={'flex-start'}
+            gap={10}>
+            <SkeletonPlaceholder.Item
+              width={100}
+              height={14}
+              borderRadius={4}
+            />
+            <SkeletonPlaceholder.Item
+              width={100}
+              height={10}
+              borderRadius={4}
+            />
+          </SkeletonPlaceholder.Item>
         </SkeletonPlaceholder.Item>
       </View>
     </SkeletonPlaceholder>
