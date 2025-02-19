b36f285c24cc79126f63e8d9771fb950c908e168
Gabriel <gabrielbazan89@gmail.com>
2025-02-19 17:12:24 -0300

    [FIX] modals touchables for android

--- src/navigation/wallet/components/DropdownOption.tsx
+++ src/navigation/wallet/components/DropdownOption.tsx
@@ -22,7 +22,7 @@ import {formatFiatAmountObj} from '../../../utils/helper-methods';
 import AngleRight from '../../../../assets/img/angle-right.svg';
 import {getRemainingWalletCount} from '../../../store/wallet/utils/wallet';
 import {WalletRowProps} from '../../../components/list/WalletRow';
-import {TouchableOpacity} from 'react-native-gesture-handler';
+import { TouchableOpacity } from 'react-native';
 
 interface Props {
   optionId: string;
