86f27c77b9e25cd0b8b0a11de72ecade92045358
Marty Alcala <marty@bitpay.com>
2025-05-30 11:15:48 -0400

    chore: upgrade React Native to 0.79.2

--- ios/BitPayApp/AppDelegate.h
+++
@@ -1,7 +0,0 @@
-#import <RCTAppDelegate.h>
-#import <BrazeUI/BrazeUI-Swift.h>
-@interface AppDelegate : RCTAppDelegate
-@property (nonatomic, assign) BOOL isBitPayAppLoaded;
-@property (nonatomic, strong) BRZInAppMessageRaw *cachedInAppMessage;
-- (void)setBitPayAppLoaded:(BOOL)loaded;
-@end
