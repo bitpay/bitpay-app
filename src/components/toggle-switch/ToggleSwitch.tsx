2f05281d280405be530250506f5b34bcc05547f2
Marty Alcala <marty@bitpay.com>
2025-09-23 12:00:00 -0400

    fix: vertically center switches on iOS 26

--- src/components/toggle-switch/ToggleSwitch.tsx
+++ src/components/toggle-switch/ToggleSwitch.tsx
@@ -1,22 +1,31 @@
 import React from 'react';
 import {Switch} from 'react-native';
+import styled from 'styled-components/native';
 import {Action, NeutralSlate, White} from '../../styles/colors';
+
 interface Props {
   onChange: ((value: boolean) => any) | undefined;
   isEnabled: boolean;
   isDisabled?: boolean;
 }
 
+const SwitchContainer = styled.View`
+  align-self: stretch;
+  justify-content: center;
+`;
+
 const ToggleSwitch = ({onChange, isEnabled, isDisabled}: Props) => {
   return (
-    <Switch
-      onValueChange={onChange}
-      value={isEnabled}
-      trackColor={{true: Action, false: NeutralSlate}}
-      thumbColor={White}
-      thumbTintColor={White}
-      disabled={isDisabled}
-    />
+    <SwitchContainer>
+      <Switch
+        onValueChange={onChange}
+        value={isEnabled}
+        trackColor={{true: Action, false: NeutralSlate}}
+        thumbColor={White}
+        thumbTintColor={White}
+        disabled={isDisabled}
+      />
+    </SwitchContainer>
   );
 };
 
