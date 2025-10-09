b0f527122167a80e4e94ff49528c1f909c1cbe13
Marty Alcala <marty@bitpay.com>
2025-10-09 16:07:09 -0400

    feat: check if gift card search query matches any values in supportedBrands

--- src/store/shop/shop.models.ts
+++ src/store/shop/shop.models.ts
@@ -56,6 +56,7 @@ export interface CommonCardConfig {
   redeemButtonText?: string;
   redeemInstructions?: string;
   redeemUrl?: string;
+  supportedBrands?: string[];
   supportedUrls?: string[];
   terms: string;
   website: string;
