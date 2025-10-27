a50f11776627d9cfc0260dcda2e804022ba864a5
Gabriel Masclef <gabrielmasclef@hotmail.com>
2025-10-27 17:08:32 -0300

    Feat: max button in amount pills

--- src/navigation/services/components/externalServicesAmountPills.tsx
+++ src/navigation/services/components/externalServicesAmountPills.tsx
@@ -1,5 +1,5 @@
 import React, {memo} from 'react';
-import styled, {useTheme} from 'styled-components/native';
+import styled from 'styled-components/native';
 import {
   Action,
   LightBlack,
@@ -13,22 +13,36 @@ import {useTranslation} from 'react-i18next';
 import {TouchableOpacity} from '../../../components/base/TouchableOpacity';
 import {formatFiatAmount} from '../../../utils/helper-methods';
 
-const AmountPillsContainer = styled.View<{isSmallScreen?: boolean}>`
+const AmountPillsContainer = styled.View<{
+  isSmallScreen?: boolean;
+  hideFiatPills?: boolean;
+}>`
   margin: 0px ${ScreenGutter};
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
+  justify-content: ${({hideFiatPills}) =>
+    hideFiatPills ? 'flex-end' : 'space-between'};
   align-items: center;
 `;
 
 const AmountPill = styled(TouchableOpacity)<{
   isSmallScreen?: boolean;
   isSelected?: boolean;
+  showMaxPill?: boolean;
+  hideFiatPills?: boolean;
 }>`
-  background-color: ${({theme: {dark}, isSelected}) =>
-    isSelected ? Action : dark ? LightBlack : NeutralSlate};
-  min-width: 31%;
-  max-width: 250px;
+  background-color: ${({theme: {dark}, isSelected, disabled}) =>
+    disabled
+      ? NeutralSlate
+      : isSelected
+      ? Action
+      : dark
+      ? LightBlack
+      : NeutralSlate};
+  min-width: ${({showMaxPill, hideFiatPills}) =>
+    showMaxPill && !hideFiatPills ? '23%' : '31%'};
+  max-width: ${({showMaxPill, hideFiatPills}) =>
+    showMaxPill && !hideFiatPills ? '187px' : '250px'};
   height: ${({isSmallScreen}) => (isSmallScreen ? 30 : 46)}px;
   display: flex;
   flex-direction: row;
@@ -41,13 +55,14 @@ const AmountPill = styled(TouchableOpacity)<{
 const AmountPillText = styled(BaseText)<{
   isSelected?: boolean;
   isSmallScreen?: boolean;
+  disabled?: boolean;
 }>`
   font-size: ${({isSmallScreen}) => (isSmallScreen ? 14 : 20)}px;
   font-weight: 400;
   line-height: ${({isSmallScreen}) => (isSmallScreen ? 18 : 30)}px;
   letter-spacing: 0px;
-  color: ${({theme: {dark}, isSelected}) =>
-    isSelected ? White : dark ? White : SlateDark};
+  color: ${({theme: {dark}, isSelected, disabled}) =>
+    disabled ? SlateDark : isSelected ? White : dark ? White : SlateDark};
 `;
 
 const defaultPills = [
@@ -64,37 +79,69 @@ const defaultPills = [
 
 export interface ExternalServicesAmountPillsProps {
   fiatCurrency: string;
-  onPillPress: ((value: number) => any) | undefined;
-  selectedValue?: number | null;
+  onPillPress: ((value: number | string) => any) | undefined;
+  selectedValue?: number | string | null;
+  showMaxPill?: boolean;
+  hideFiatPills?: boolean;
+  maxPillDisabled?: boolean;
 }
 
 const ExternalServicesAmountPills: React.FC<
   ExternalServicesAmountPillsProps
-> = ({fiatCurrency, onPillPress, selectedValue}) => {
-  const theme = useTheme();
+> = ({
+  fiatCurrency,
+  onPillPress,
+  selectedValue,
+  showMaxPill,
+  hideFiatPills,
+  maxPillDisabled,
+}) => {
   const {t} = useTranslation();
 
   const _isSmallScreen = HEIGHT < 700;
   return (
-    <AmountPillsContainer isSmallScreen={_isSmallScreen}>
-      {defaultPills.map(pill => {
-        const isSelected = selectedValue === pill.value;
-        return (
-          <AmountPill
-            onPress={() => onPillPress?.(pill.value)}
-            key={pill.value}
-            isSmallScreen={_isSmallScreen}
-            isSelected={isSelected}>
-            <AmountPillText
-              isSelected={isSelected}
-              isSmallScreen={_isSmallScreen}>
-              {formatFiatAmount(Number(pill.value), fiatCurrency, {
-                customPrecision: 'minimal',
-              })}
-            </AmountPillText>
-          </AmountPill>
-        );
-      })}
+    <AmountPillsContainer
+      isSmallScreen={_isSmallScreen}
+      hideFiatPills={hideFiatPills}>
+      {!hideFiatPills
+        ? defaultPills.map(pill => {
+            const isSelected = selectedValue === pill.value;
+            return (
+              <AmountPill
+                onPress={() => onPillPress?.(pill.value)}
+                key={pill.value}
+                isSmallScreen={_isSmallScreen}
+                isSelected={isSelected}
+                showMaxPill={showMaxPill}
+                hideFiatPills={hideFiatPills}>
+                <AmountPillText
+                  isSelected={isSelected}
+                  isSmallScreen={_isSmallScreen}>
+                  {formatFiatAmount(Number(pill.value), fiatCurrency, {
+                    customPrecision: 'minimal',
+                  })}
+                </AmountPillText>
+              </AmountPill>
+            );
+          })
+        : null}
+      {showMaxPill ? (
+        <AmountPill
+          disabled={maxPillDisabled}
+          onPress={() => onPillPress?.('max')}
+          key={'max'}
+          isSmallScreen={_isSmallScreen}
+          isSelected={selectedValue === 'max'}
+          showMaxPill={showMaxPill}
+          hideFiatPills={hideFiatPills}>
+          <AmountPillText
+            disabled={maxPillDisabled}
+            isSelected={selectedValue === 'max'}
+            isSmallScreen={_isSmallScreen}>
+            {t('MAX')}
+          </AmountPillText>
+        </AmountPill>
+      ) : null}
     </AmountPillsContainer>
   );
 };
