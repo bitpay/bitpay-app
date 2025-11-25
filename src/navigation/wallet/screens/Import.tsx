ee3de3b06a83ebb2a60ef1168cce9ff7ffbfdfe3
Gustavo Cortez <cmgustavo83@gmail.com>
2025-11-25 16:16:59 -0300

    Feat: implement sensitive input clearing on app state change
    # Conflicts:
    #	src/navigation/wallet/components/FileOrText.tsx

--- src/navigation/wallet/screens/Import.tsx
+++ src/navigation/wallet/screens/Import.tsx
@@ -8,7 +8,6 @@ import {WalletGroupParamList, WalletScreens} from '../WalletGroup';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {useTranslation} from 'react-i18next';
 import CustomTabBar from '../../../components/custom-tab-bar/CustomTabBar';
-import {Platform} from 'react-native';
 
 type ImportScreenProps = NativeStackScreenProps<
   WalletGroupParamList,
