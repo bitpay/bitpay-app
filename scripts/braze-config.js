33e61fab4ec013a5f8feed23a70819921d224588
Gustavo Cortez <cmgustavo83@gmail.com>
2026-05-22 16:02:11 -0300

    Braze: Chore - Upgrade to latest plugin version

--- scripts/braze-config.js
+++ scripts/braze-config.js
@@ -15,27 +15,9 @@ const dotenv = require('dotenv');
     throw result.error;
   }
 
-  // Configure Braze for iOS
-  if (process.env.BRAZE_API_KEY_IOS) {
-    const brazeConfigFileIOS = `${__dirname}/../ios/BitPayApp/AppDelegate.swift`;
-    let contentiOS = fs.readFileSync(brazeConfigFileIOS, 'utf8');
-    contentiOS = contentiOS.replace(
-      'BRAZE_API_KEY_REPLACE_ME',
-      process.env.BRAZE_API_KEY_IOS,
-    );
-    fs.writeFileSync(brazeConfigFileIOS, contentiOS);
-  }
-
   // Configure Braze for Android
   const brazeConfigFileAndroid = `${__dirname}/../android/app/src/main/res/values/braze.xml`;
   let contentAndroid = fs.readFileSync(brazeConfigFileAndroid, 'utf8');
-
-  if (process.env.BRAZE_API_KEY_ANDROID) {
-    contentAndroid = contentAndroid.replace(
-      'BRAZE_API_KEY_REPLACE_ME',
-      process.env.BRAZE_API_KEY_ANDROID,
-    );
-  }
 
   if (process.env.BRAZE_SENDER_ID) {
     contentAndroid = contentAndroid.replace(
