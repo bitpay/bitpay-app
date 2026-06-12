ed6cf2670c6800df5593227c47f60e239febe2eb
Gabriel <gabrielbazan89@gmail.com>
2026-06-12 15:38:30 -0300

    Sentry: fix - chain native exception handler to Sentrys uncaughtExceptionHandler

--- index.js
+++ index.js
@@ -166,7 +166,8 @@ const ReduxProvider = () => {
       setStore({store, persistor});
       setStoreReady(true);
       setJSExceptionHandler(makeErrorHandler(store), true);
-      setNativeExceptionHandler(makeNativeExceptionHandler(store));
+      // executeDefaultHandler=true chains to Sentry's UncaughtExceptionHandler so native crashes are captured
+      setNativeExceptionHandler(makeNativeExceptionHandler(store), true, true);
     });
 
     return () => {
