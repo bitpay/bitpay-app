b0c8e98d5f583c9aa1c57f6c17c1d016d7529979
Gabriel <gabrielbazan89@gmail.com>
2026-07-14 11:46:15 -0300

    Moralis: Enhancement - migrate client requests to go through BWC/BWS

--- declarations.d.ts
+++ declarations.d.ts
@@ -20,7 +20,6 @@ declare module '@env' {
   export const DOSH_WHITELIST: string;
   export const GIT_COMMIT_HASH: string;
   export const MIXPANEL_PROJECT_TOKEN: string;
-  export const MORALIS_API_KEY: string;
   export const WALLET_CONNECT_V2_PROJECT_ID: string;
   export const ZENLEDGER_CLIENT_ID: string;
   export const ZENLEDGER_CLIENT_SECRET: string;
