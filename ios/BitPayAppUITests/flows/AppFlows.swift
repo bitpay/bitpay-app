0bf5908fc172dfd79efb34659ec215254be15696
vinotestlio <vinoth@testlio.com>
2026-08-11 10:33:28 +0530

    app flows updated

--- ios/BitPayAppUITests/flows/AppFlows.swift
+++ ios/BitPayAppUITests/flows/AppFlows.swift
@@ -8,32 +8,36 @@
 import XCTest
 
 final class AppFlows {
-
-    static func completeOnboardingIfRequired(app: XCUIApplication) {
-
-        let onboardingPage = OnboardingPage(app: app)
-
-        if onboardingPage.isContinueWithoutAccountButtonDisplayed() {
-
-            onboardingPage.tapContinuewithoutAnAccount()
-            onboardingPage.skipOnboarding()
-            onboardingPage.skipOnboarding()
-            onboardingPage.createWallet()
-
-            guard onboardingPage.isBackupKeyLabelDisplayed() else {
-                XCTFail("Backup key page did not appear")
-                return
-            }
-
-            onboardingPage.skipBackup()
-            onboardingPage.tapLater()
-            onboardingPage.acceptTerms()
-            onboardingPage.tapAgreeAndContinueButton()
-
-            XCTAssertTrue(
-                onboardingPage.isYourPortfolioBalanceTextDisplayed(),
-                "Home screen did not load"
-            )
-        }
+  
+  static func completeOnboardingIfRequired(app: XCUIApplication) {
+    
+    let onboardingPage = OnboardingPage(app: app)
+    
+    if onboardingPage.isContinueWithoutAccountButtonDisplayed() {
+      
+      onboardingPage.tapContinuewithoutAnAccount()
+      
+      onboardingPage.skipOnboarding()
+      Thread.sleep(forTimeInterval: 2.0)
+      onboardingPage.skipOnboarding()
+      
+      onboardingPage.createWallet()
+      
+      guard onboardingPage.isBackupKeyLabelDisplayed() else {
+        XCTFail("Backup key page did not appear")
+        return
+      }
+      
+      onboardingPage.skipBackup()
+      Thread.sleep(forTimeInterval: 2.0)
+      onboardingPage.tapLater()
+      onboardingPage.acceptTerms()
+      onboardingPage.tapAgreeAndContinueButton()
+      
+      XCTAssertTrue(
+        onboardingPage.isYourPortfolioBalanceTextDisplayed(),
+        "Home screen did not load"
+      )
     }
+  }
 }
