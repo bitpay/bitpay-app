49f812ff3da1e67b7db5c455cb882dada7d4fe16
johnathan White <jwhite@bitpay.com>
2026-08-13 11:52:09 -0400

    bump 14.45

--- src/navigation/wallet/screens/send/SendTo.tsx
+++ src/navigation/wallet/screens/send/SendTo.tsx
@@ -154,8 +154,8 @@ const isEmailAddress = (text: string) => {
   if (!text.includes('@')) {
     return false;
   }
-  const reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
-  return reg.test(text);
+
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(text);
 };
 
 export const BuildKeyAccountRow = (
